a778e33ca737ceab54e92699270e6aa96d043eb9
rgast <rgast@cbs.mpg.de>
2020-07-09 08:39:52 +0200

    - updated bifurcation_stn_gpe_syns.py to the new interface of `codim2_search()`

--- BasalGanglia/bifurcation_stn_gpe_syns.py
+++ BasalGanglia/bifurcation_stn_gpe_syns.py
@@ -567,8 +567,9 @@ if any(c3):
         ########################################
 
         c3_b3_cd2_1 = codim2_search(params=[26, 22], starting_points=['HB1'], origin=c3_b3_cont, pyauto_instance=a,
-                                    periodic=False, c='qif2', NDIM=n_dim, NPAR=n_params, RL0=0.1, RL1=10.0, NMX=8000,
-                                    DSMAX=0.05, max_recursion_depth=4, name="c3.2")
+                                    periodic=False, c='qif', NDIM=n_dim, NPAR=n_params, RL0=0.1, RL1=10.0, NMX=8000,
+                                    DSMAX=0.05, max_recursion_depth=3, name="c3.2", kwargs_2D_lc_cont={'c': 'qif3'},
+                                    kwargs_2D_cont={'c': 'qif2'}, kwargs_lc_cont={'c': 'qif2b'})
 
         # save results
         kwargs = {'k_gp/k_gp_e:names': list(c3_b3_cd2_1.keys())}
@@ -579,7 +580,7 @@ if any(c3):
 
         c3_b3_cd2_2 = codim2_search(params=[24, 22], starting_points=['HB1'], origin=c3_b3_cont, pyauto_instance=a,
                                     periodic=False, c='qif2', NDIM=n_dim, NPAR=n_params, RL0=0.1, RL1=10.0, NMX=8000,
-                                    DSMAX=0.05, max_recursion_depth=4, name="c3.2")
+                                    DSMAX=0.05, max_recursion_depth=3, name="c3.2")
 
         # save results
         kwargs = {'k_gp/k_gp_e:names': list(c3_b3_cd2_1.keys()),
@@ -591,7 +592,7 @@ if any(c3):
 
         c3_b3_cd2_3 = codim2_search(params=[26, 24], starting_points=['HB1'], origin=c3_b3_cont, pyauto_instance=a,
                                     periodic=False, c='qif2', NDIM=n_dim, NPAR=n_params, RL0=0.1, RL1=10.0, NMX=8000,
-                                    DSMAX=0.05, max_recursion_depth=4)
+                                    DSMAX=0.05, max_recursion_depth=3)
 
         # save results
         kwargs = {'k_gp/k_gp_e:names': list(c3_b3_cd2_1.keys()),
