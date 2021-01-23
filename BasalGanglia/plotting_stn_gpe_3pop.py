ec8b817772d593d584c0f03c70617e59f4b91789
rgast <rgast@cbs.mpg.de>
2021-01-23 12:56:43 +0100

    - improved plot aesthetics of stn-gpe plots

--- BasalGanglia/plotting_stn_gpe_3pop.py
+++ BasalGanglia/plotting_stn_gpe_3pop.py
@@ -24,13 +24,16 @@ a = PyAuto.from_file(f"results/{fname}_conts.pkl", auto_dir=auto_dir)
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rc('text', usetex=True)
 plt.rcParams['figure.constrained_layout.use'] = True
-plt.rcParams['figure.dpi'] = 100
+plt.rcParams['figure.dpi'] = 200
 plt.rcParams['figure.figsize'] = (5.25, 4.0)
 plt.rcParams['font.size'] = 8.0
 plt.rcParams['axes.titlesize'] = 8.0
 plt.rcParams['axes.labelsize'] = 8.0
 plt.rcParams['lines.linewidth'] = 0.7
-markersize = 40
+plt.rcParams['axes.titlepad'] = 0.0
+labelpad = 1.0
+plt.rcParams['axes.labelpad'] = labelpad
+markersize = 25
 
 ############
 # plotting #
@@ -38,30 +41,30 @@ markersize = 40
 
 # create figure layout
 fig = plt.figure(1)
-grid = gridspec.GridSpec(nrows=5, ncols=8, figure=fig)
+grid = gridspec.GridSpec(nrows=5, ncols=12, figure=fig)
 
 # 2d: k_pe x k_gp
-ax = fig.add_subplot(grid[:3, :3])
+ax = fig.add_subplot(grid[:3, :5])
 ax = a.plot_continuation('PAR(22)', 'PAR(5)', cont=f'k_gp/k_pe:lp1', ax=ax, line_color_stable='#3689c9',
                          line_color_unstable='#3689c9', default_size=markersize,
-                         line_style_unstable='solid', ignore=['UZ', 'GH'])
+                         line_style_unstable='solid', ignore=['UZ', 'GH', 'ZH'])
 ax = a.plot_continuation('PAR(22)', 'PAR(5)', cont=f'k_gp/k_pe:hb1', ax=ax, line_color_stable='#148F77',
                          line_color_unstable='#148F77', default_size=markersize,
-                         line_style_unstable='solid', ignore=['UZ', 'GH'])
+                         line_style_unstable='solid', ignore=['UZ', 'GH', 'ZH'])
 ax = a.plot_continuation('PAR(22)', 'PAR(5)', cont=f'k_gp/k_pe:hb2', ax=ax, line_color_stable='#ee2b2b',
                          line_color_unstable='#ee2b2b', default_size=markersize,
-                         line_style_unstable='solid', ignore=['UZ', 'GH'])
+                         line_style_unstable='solid', ignore=['UZ', 'GH', 'ZH'])
 # ax = a.plot_continuation('PAR(22)', 'PAR(5)', cont=f'k_gp/k_pe:hb3', ax=ax, line_color_stable='#ee2b2b',
 #                          line_color_unstable='#ee2b2b', default_size=markersize,
 #                          line_style_unstable='solid', ignore=['UZ'])
-ax.set_ylabel(r'$k_{pe}$')
-ax.set_xlabel(r'$k_{gp}$')
+ax.set_ylabel(r'$k_{pe}$', labelpad=labelpad)
+ax.set_xlabel(r'$k_{gp}$', labelpad=labelpad)
 ax.set_title('2D bifurcation diagram')
 ax.set_xlim([0.0, 20.0])
 ax.set_ylim([0.0, 25.0])
 
 # 1D continuation in k_pe for k_gp = 5.0
-ax = fig.add_subplot(grid[0, 3:])
+ax = fig.add_subplot(grid[0, 5:])
 ax = a.plot_continuation('PAR(5)', 'U(3)', cont=f'k_pe:1', ax=ax, default_size=markersize, ignore=['UZ', 'GH'])
 ax = a.plot_continuation('PAR(5)', 'U(3)', cont=f'k_pe:1:lc1', ax=ax, default_size=markersize, ignore=['UZ'],
                          line_color_stable='#148F77', line_color_unstable='#148F77')
@@ -69,26 +72,30 @@ ax = a.plot_continuation('PAR(5)', 'U(3)', cont=f'k_pe:1:lc2', ax=ax, default_si
                          line_color_stable='#148F77', line_color_unstable='#148F77')
 # ax = a.plot_continuation('PAR(5)', 'U(3)', cont=f'k_pe:1:lc3', ax=ax, default_size=markersize, ignore=['UZ'],
 #                          line_color_stable='#148F77', line_color_unstable='#148F77')
-ax.set_xlabel('$k_{pe}$')
-ax.set_ylabel('')
+ax.set_xlabel('$k_{pe}$', labelpad=labelpad)
+ax.set_ylabel('', labelpad=labelpad)
 ax.set_title('1D bifurcation diagrams')
 ax.set_xlim([0.0, 5.0])
 ax.set_ylim([0.0, 0.15])
+ax.set_yticks([0, 0.06, 0.12])
+ax.set_yticklabels(["0", "60", "120"])
 
 # 1D continuation in k_gp for k_pe = 8.0
-ax = fig.add_subplot(grid[1, 3:])
+ax = fig.add_subplot(grid[1, 5:])
 ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:1', ax=ax, default_size=markersize, ignore=['UZ'])
 ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:1:lc1', ax=ax, default_size=markersize, ignore=['UZ'],
                          line_color_stable='#148F77', line_color_unstable='#148F77')
 ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:1:lc2', ax=ax, default_size=markersize, ignore=['UZ'],
                          line_color_stable='#148F77', line_color_unstable='#148F77')
-ax.set_xlabel('$k_{gp}$')
-ax.set_ylabel('r')
+ax.set_xlabel('$k_{gp}$', labelpad=labelpad)
+ax.set_ylabel('r', labelpad=labelpad)
 ax.set_xlim([1.0, 13.0])
 ax.set_ylim([0.0, 0.2])
+ax.set_yticks([0, 0.1, 0.2])
+ax.set_yticklabels(["0", "100", "200"])
 
 # 1D continuation in k_gp for k_pe = 16.0
-ax = fig.add_subplot(grid[2, 3:])
+ax = fig.add_subplot(grid[2, 5:])
 ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:2', ax=ax, default_size=markersize, ignore=['UZ'])
 ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:2:lc1', ax=ax, default_size=markersize, ignore=['UZ'],
                          line_color_stable='#148F77', line_color_unstable='#148F77')
@@ -96,10 +103,12 @@ ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:2:lc2', ax=ax, default_s
                          line_color_stable='#148F77', line_color_unstable='#148F77')
 # ax = a.plot_continuation('PAR(22)', 'U(3)', cont=f'k_gp:2:lc3', ax=ax, default_size=markersize, ignore=['UZ'],
 #                          line_color_stable='#148F77', line_color_unstable='#148F77')
-ax.set_xlabel('$k_{gp}$')
-ax.set_ylabel('')
+ax.set_xlabel('$k_{gp}$', labelpad=labelpad)
+ax.set_ylabel('', labelpad=labelpad)
 ax.set_xlim([13.5, 15.5])
 ax.set_ylim([0.0, 0.1])
+ax.set_yticks([0, 0.05, 0.1])
+ax.set_yticklabels(["0", "50", "100"])
 
 # plt time series and psd profiles
 ##################################
@@ -107,7 +116,7 @@ ax.set_ylim([0.0, 0.1])
 rates = data['results']
 map = data['map']
 psds = data['psds']
-ax_indices = [(3, 0), (3, 4), (4, 0), (4, 4)]
+ax_indices = [(3, 0), (3, 6), (4, 0), (4, 6)]
 
 for i, key in enumerate(map.index):
 
@@ -118,27 +127,29 @@ for i, key in enumerate(map.index):
     col = ax_indices[i][1]
 
     # plot timeseries
-    ax1 = fig.add_subplot(grid[row, col:col+2])
+    ax1 = fig.add_subplot(grid[row, col:col+4])
     ax1.plot(rates.index[100000:105000], rates.loc[10.0:10.49999, ('r_e', key)])
     ax1.plot(rates.index[100000:105000], rates.loc[10.0:10.49999, ('r_i', key)])
     #ax1.plot(rates.index[100000:105000], rates.loc[10.0:10.49999, ('r_a', key)])
-    ax1.set_ylabel('r')
-    ax1.set_title(f'{i+1}', loc='left')
+    ax1.set_title('r')
     #ax1.set_ylim([18.0, 180.0])
 
     # plot psd
-    ax2 = fig.add_subplot(grid[row, col+2:col+4])
+    ax2 = fig.add_subplot(grid[row, col+4:col+6])
     ax2.plot(psds['freq_stn'][i], psds['pow_stn'][i])
     ax2.plot(psds['freq_gpe'][i], psds['pow_gpe'][i])
-    ax2.set_ylabel('PSD')
+    ax2.set_title('PSD')
+    ax2.set_in_layout(False)
+    ax2.set_xlim([0.0, 120.0])
+    ax2.set_xticks([0, 50, 100])
     #ax2.set_ylim([-5.0, 275] if i == 2 else [-5.0, 50])
 
 ax1.set_xlabel('time in ms')
 ax2.set_xlabel('f in Hz')
 
 # padding
-fig.set_constrained_layout_pads(w_pad=0.01, h_pad=0.03, hspace=0., wspace=0.)
+fig.set_constrained_layout_pads(w_pad=0.03, h_pad=0.01, hspace=0., wspace=0.)
 
 fig.canvas.draw()
-#plt.savefig(f'results/{fname}_bf.svg')
+plt.savefig(f'results/{fname}_bf.svg')
 plt.show()
