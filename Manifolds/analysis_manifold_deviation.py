6601bb7688ef65000b8343a6e0d30bde2a15c97c
Richert <richgast.90@gmail.com>
2022-06-30 14:31:25 -0500

    new simulation and plotting scripts for the manifold project

---
+++ Manifolds/analysis_manifold_deviation.py
@@ -0,0 +1,24 @@
+import numpy as np
+from pandas import DataFrame
+import pickle
+
+# calculate variance around mean-field manifold
+N = 7
+conditions = np.arange(0, N)
+D = np.zeros((N, N))
+index = []
+columns = []
+for n in conditions:
+    res = pickle.load(open(f"results/rnn_simulations/rnn_{n}.p", "rb"))
+    for m in range(len(res['in_var'])):
+        r = res['results'][m]['v']
+        D[n, m] = np.var(r)
+    index.append(res['p'])
+    if n == 0:
+        columns.extend(res['in_var'])
+
+# store results in Dataframe
+results = DataFrame(data=D, index=index, columns=columns)
+
+# save results
+results.to_pickle("results/manifold_deviations.p")
