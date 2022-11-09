9ec76342f45f670284de699644151f604b440b4f
Richert <richgast.90@gmail.com>
2022-11-09 11:19:29 -0600

    new scripts for reservoir computing stuff

--- Manifolds/simulation_snn.py
+++ Manifolds/simulation_snn.py
@@ -7,20 +7,23 @@ import pickle
 # model definition
 ##################
 
+# file name for saving
+fname = "snn_data3"
+
 # network parameters
 N = 1000
-p = 0.1
+p = 0.2
 C = 100.0
 k = 0.7
 v_r = -60.0
 v_t = -40.0
-Delta = 0.5
+Delta = 1.0
 eta = 50.0
-sigma = 5.0
+sigma = 10.0
 a = 0.03
 b = -2.0
 d = 100.0
-g = 15.0
+g = 5.0
 E_r = 0.0
 tau_s = 6.0
 v_spike = 1000.0
@@ -42,12 +45,12 @@ dt = 1e-2
 steps = int(T/dt)
 sampling_steps = 100
 I_ext = np.random.randn(steps, 1)*sigma*np.sqrt(dt)
-W_in = input_connections(N, 1, 1.0, variance=2.0, zero_mean=False)
+W_in = input_connections(N, 1, 1.0, variance=1.0, zero_mean=False)
 
 # initialize model
-net = Network.from_yaml("neuron_model_templates.spiking_neurons.ik.iku", weights=J, source_var="s", target_var="s_in",
+net = Network.from_yaml("neuron_model_templates.spiking_neurons.ik.ik", weights=J, source_var="s", target_var="s_in",
                         input_var="I_ext", output_var="s", spike_var="spike", spike_def="v", node_vars=node_vars,
-                        op="iku_op", spike_reset=v_reset, spike_threshold=v_spike, dt=dt)
+                        op="ik_op", spike_reset=v_reset, spike_threshold=v_spike, dt=dt)
 net.add_input_layer(1, W_in, trainable=False)
 
 # simulation
@@ -60,7 +63,7 @@ obs = net.run(inputs=I_ext, device="cpu", sampling_steps=sampling_steps, record_
 inp = pd.DataFrame(index=obs["v"].index, data=I_ext[::sampling_steps, :])
 pickle.dump({"s": obs["out"], "v": obs["v"], "J": J, "etas": etas, "I_ext": inp, "W_in": W_in, "params": node_vars,
              "dt": dt, "sr": sampling_steps, "v_reset": v_reset, "v_spike": v_spike},
-            open("results/snn_data.pkl", "wb"))
+            open(f"results/{fname}.pkl", "wb"))
 
 # exemplary plotting
 s = obs["out"]
--- Manifolds/training_snn.py
+++ Manifolds/training_snn.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import pickle
 
 # load data
-data = pickle.load(open("results/snn_data.pkl", "rb"))
+fname = "snn_data3"
+data = pickle.load(open(f"results/{fname}.pkl", "rb"))
 I_ext = data["I_ext"].loc[:, 0]
 
 # create target data for different taus
@@ -15,7 +16,7 @@ dt = data["dt"]
 time = (I_ext.index - np.min(I_ext.index)) * dt
 
 # create target data
-taus = [2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0]
+taus = [1.0, 2.0, 4.0, 8.0, 16.0, 32.0, 64.0, 128.0]
 epsilon = 1e-5
 targets = []
 for tau in taus:
@@ -28,23 +29,24 @@ for tau in taus:
 
 # training procedure
 cutoff = 1000
-train_split = 6000
+plot_length = 1000
 s = data["s"].iloc[cutoff:-cutoff, :]
 scores = []
-for target in targets:
+for tau, target in zip(taus, targets):
 
     # ridge regression
-    res = readout(s, target[cutoff:-cutoff], train_split=6000)
+    res = readout(s, target[cutoff:-cutoff])
 
     # plotting
-    plt.plot(res["target"], color="black", linestyle="dashed")
-    plt.plot(res["prediction"], color="orange")
+    plt.plot(res["target"][-plot_length:], color="black", linestyle="dashed")
+    plt.plot(res["prediction"][-plot_length:], color="orange")
     plt.legend(["target", "prediction"])
+    plt.title(f"tau = {tau}, score = {res['train_score']}")
     plt.show()
 
-    scores.append(res["test_score"])
+    scores.append(res["train_score"])
 
 # save data to file
 data["taus"] = taus
 data["scores"] = scores
-pickle.dump(data, open("results/snn_data.pkl", "wb"))
+pickle.dump(data, open(f"results/{fname}.pkl", "wb"))
