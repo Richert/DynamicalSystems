31a8c8c60c1dce7e98f546f220b7d8505f56d744
Richert <richgast.90@gmail.com>
2022-11-22 12:44:34 -0600

    updates to EIC bifurcation analysis

--- Manifolds/plotting_manifolds2.py
+++ Manifolds/plotting_manifolds2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pickle
+from matplotlib import gridspec
 import matplotlib.pyplot as plt
-from os import listdir
 from pyrecu import sort_via_modules
 from matplotlib import cm
 from matplotlib.patches import Rectangle
@@ -13,7 +13,7 @@ plt.rcParams["font.family"] = "Roboto"
 # plt.rc('text', usetex=True)
 #plt.rcParams['figure.constrained_layout.use'] = True
 plt.rcParams['figure.dpi'] = 200
-plt.rcParams['figure.figsize'] = (12, 8)
+plt.rcParams['figure.figsize'] = (6, 5)
 plt.rcParams['font.size'] = 14.0
 plt.rcParams['axes.titlesize'] = 16
 plt.rcParams['axes.labelsize'] = 16
@@ -32,7 +32,7 @@ deltas = np.arange(0.2, 4.1, 0.2)
 
 
 # collect data
-example_idx = 0
+example_idx = 3
 modules, standard_errors, module_ex = [], [], []
 for n in range(n_files):
     data = pickle.load(open(f"{path}/rnn_{n}.p", "rb"))
@@ -45,49 +45,46 @@ for n in range(n_files):
                       data['nodes'][example_idx], data['v'][example_idx]))
 
 # get example modules
-idx = np.argmin(np.abs(deltas - 2.0))
+idx = np.argmin(np.abs(deltas - 1.5))
 mods, A, nodes, signal = module_ex[idx]
 C = sort_via_modules(A, mods)
 
 # plot data
-fig, axes = plt.subplots(ncols=3, nrows=2)
+# create figure layout
+fig = plt.figure(1)
+grid = gridspec.GridSpec(nrows=3, ncols=2, figure=fig)
 
-ax1 = axes[0, 2]
-ax1.bar(deltas, standard_errors, color='grey')
+ax1 = fig.add_subplot(grid[2, 0])
+ax1.plot(deltas, [m[0] for m in modules])
+ax1.fill_between(deltas, [m[0] - m[1] for m in modules], [m[0] + m[1] for m in modules], alpha=0.3)
+ax1.set_xticks(deltas[::6])
 ax1.set_xlabel(r'$\Delta_v$')
-ax1.set_ylabel('SEM')
+ax1.set_ylabel('# communities')
 
-ax2 = axes[1, 0]
-ax2.plot(deltas, [m[0] for m in modules])
-ax2.fill_between(deltas, [m[0] - m[1] for m in modules], [m[0] + m[1] for m in modules], alpha=0.3)
-ax2.set_xticks(deltas[::6])
-ax2.set_xlabel(r'$\Delta_v$')
-ax2.set_ylabel('# communities')
-
-ax3 = axes[1, 1]
+ax2 = fig.add_subplot(grid[:2, 1])
 C1 = C[:, :]
 C1[C1 > 0] = 1.0
-ax3.imshow(C1, cmap='magma', interpolation='none')
-ax3.set_xlabel('neuron id')
-ax3.set_ylabel('neuron id')
-ax3.set_xticks(np.arange(0, 900, 400))
+ax2.imshow(C1, cmap='magma', interpolation='none')
+ax2.set_xlabel('neuron id')
+ax2.set_ylabel('neuron id')
+ax2.set_xticks(np.arange(0, 900, 400))
 
-ax4 = axes[1, 2]
+ax3 = fig.add_subplot(grid[2, 1])
 cmap = cm.get_cmap('tab10')
 x, y = 0, 0
 for key, (indices, _) in mods.items():
     mean_signal = gaussian_filter1d(np.mean(signal[9000:9500, nodes[indices]], axis=1), sigma=10)
-    ax4.plot(mean_signal, c=cmap(key-1))
+    ax3.plot(mean_signal, c=cmap(key-1))
     inc = len(indices)
     rect = Rectangle([x, y], inc, inc, edgecolor=cmap(key-1), facecolor='none')
-    ax3.add_patch(rect)
+    ax2.add_patch(rect)
     x += inc
     y += inc
 
 sig = gaussian_filter1d(np.mean(signal[9000:9500, :], axis=1), sigma=10)
-ax4.plot(sig, c='black')
-ax4.set_xlabel('time (ms)')
-ax4.set_ylabel('v (mV)')
+ax3.plot(sig, c='black')
+ax3.set_xlabel('time (ms)')
+ax3.set_ylabel('v (mV)')
 plt.tight_layout()
 
 # saving/plotting
