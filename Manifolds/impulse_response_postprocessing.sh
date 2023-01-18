bf4b2a6a15db2a4f75686ea0b7e21c1641101ece
Richert <richgast.90@gmail.com>
2023-01-18 14:30:14 -0600

    updated bash script for postprocessing

--- Manifolds/impulse_response_postprocessing.sh
+++ Manifolds/impulse_response_postprocessing.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-n_files = 77
-for idx in seq 0 n_files-1
+for IDX in $(seq 0 76)
 do
-  python function_generation_snn.py idx
-  python training_impulse_response.py idx
+ 	python function_generation_snn.py $IDX
+ 	python training_impulse_response.py $IDX
 done
