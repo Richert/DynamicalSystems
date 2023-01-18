67594145ba78a116ef0f5a3af972d1e72f26d0d4
Richert <richgast.90@gmail.com>
2023-01-18 15:49:03 -0600

    updates to the bash script

--- Manifolds/impulse_response_postprocessing.sh
+++ Manifolds/impulse_response_postprocessing.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-for IDX in $(seq 0 76)
+# set condition
+p1="Delta"
+p2="alpha"
+path="/home/rgf3807/data/ir_${p1}_${p2}"
+n=77
+range_end=$(($n-1))
+
+# execute python scripts
+for IDX in `seq 0 $range_end`;
 do
- 	python function_generation_snn.py $IDX
- 	python training_impulse_response.py $IDX
+ 	python function_generation_snn.py $IDX $p1 $p2 $path
+ 	python training_impulse_response.py $IDX $p1 $p2 $path
 done
