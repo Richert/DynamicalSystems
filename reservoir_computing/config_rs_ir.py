41511476a8b42963c4aee167869d1e15f4986c4a
Richert <richgast.90@gmail.com>
2023-01-27 17:03:57 -0600

    new scripts for reservoir computing stuff

--- reservoir_computing/config_rs_ir.py
+++ reservoir_computing/config_rs_ir.py
@@ -1,21 +1,51 @@
 import numpy as np
 import pickle
 from rectipy.utility import input_connections, random_connectivity, circular_connectivity
-from scipy.stats import rv_discrete
-fname = 'rs_ir_config.pkl'
+from scipy.stats import rv_discrete, cauchy
+from scipy.ndimage import convolve1d
 
 
 def dist(x: int, method: str = "inverse") -> float:
-    switch method:
-    case
-    return
+    if method == "inverse":
+        return 1/x if x > 0 else 1
+    if method == "inverse_squared":
+        return 1/x**2 if x > 0 else 1
+    if method == "exp":
+        return np.exp(-x)
+    else:
+        raise ValueError("Invalid method.")
 
+
+def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = cauchy.rvs(loc=eta, scale=delta)
+        while s <= lb or s >= ub:
+            s = cauchy.rvs(loc=eta, scale=delta)
+        samples[i] = s
+    return samples
+
+
+def alpha(t: np.ndarray, tau: float):
+    """Alpha kernel function.
+
+    :param t: time in arbitrary units. Can be a vector or a single scalar.
+    :param tau: Time constant of the alpha kernel. Must be a scalar.
+    :return: Value of the kernel function at each entry in `t`.
+    """
+    return g * t * np.exp(1 - t / tau) / tau
+
+
+# file name
+###########
+
+fname = 'rs_ir'
 
 # simulation parameters
 #######################
 
 T = 21000.0
-dt = 1e-2
+dt = 1e-1
 sr = 10
 cutoff = 1000.0
 
@@ -27,50 +57,85 @@ p = 0.1
 m = 5
 
 # setup connectivity matrix
-dist = rv_discrete(values=[])
-C = circular_connectivity(N, p, spatial_distribution=norm(loc=0.0, scale=1.0).rvs)
+indices = np.arange(0, N, dtype=np.int32)
+pdfs = np.asarray([dist(idx, method="inverse") for idx in indices])
+pdfs /= np.sum(pdfs)
+W = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
 
 # setup input matrix
-p_in = 0.05
-W_in = create_input_matrix(N, m, p_in)
+p_in = 0.1
+W_in = input_connections(N, m, p_in)
 print(np.sum(W_in, axis=0))
+
+# Default model parameters
+##########################
+
+C = 100.0
+k = 0.7
+v_r = -60.0
+v_t = -40.0
+Delta = 1.0
+eta = 40.0
+a = 0.03
+b = -2.0
+d = 130.0
+g = 8.0
+E_r = 0.0
+tau_s = 6.0
+v_spike = 1000.0
+v_reset = -1000.0
+
+# collect remaining model parameters
+node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": lorentzian(N, v_t, Delta, v_r, 2 * v_t - v_r),
+             "eta": eta, "tau_u": 1/a, "b": b, "kappa": d, "g": g, "E_r": E_r, "tau_s": tau_s, "v": v_t}
 
 # define network input
 ######################
 
-n_epochs = 10
-input_rate = 30.0/100.0
+# input parameters
+steps = int(T/dt)
+mean_isi = int(1000.0/dt)
+start = cutoff
+stim_dur = int(50.0/dt)
+stim_rate = 0.06
+def_rate = 0.003
+isi_std = 50.0
 
-steps = int(np.round(T/dt))
-store_steps = int(np.round((T - cutoff)/dts))
+# define stimulation times
+stim_times = []
+idx = start
+while idx < steps:
+    if idx > 0:
+        stim_times.append(idx)
+    idx += mean_isi + int(np.random.randn() * isi_std)
 
-inp_start = int(np.round(start/dt))
-epoch_steps = int(np.floor((steps-inp_start)/n_epochs))
-input_dur = int(np.floor(epoch_steps/m))
+# create background noise
+inp = np.zeros((steps, m))
+for c in range(m):
+    inp[:, c] = np.random.poisson(def_rate, (steps,))
 
-store_start = 0
-store_epoch = int(np.floor((store_steps-store_start)/n_epochs))
-store_dur = int(np.floor(store_epoch/m))
+# create stimuli
+stim_channels = []
+for idx in stim_times:
 
-inp = np.zeros((m, steps))
-targets = np.zeros((m, store_steps))
+    # add poisson spike trains to input array at defined stimulation times
+    if idx+stim_dur < steps:
+        idx_time = np.arange(idx, idx+stim_dur, dtype=np.int32)
+        n_inputs = np.random.randint(1, m-1)
+        channels = np.random.randint(0, m, size=n_inputs)
+        for c in channels:
+            spike_train = np.random.poisson(stim_rate, (stim_dur,))
+            inp[idx_time, c] = spike_train
+        stim_channels.append(channels)
 
-for i in range(n_epochs):
-    for j in range(m):
-
-        # generate spike train
-        spike_train = np.random.poisson(input_rate, (input_dur,))
-
-        # add spike train to input array
-        idx = np.arange(inp_start+(i*m+j)*input_dur, inp_start+(i*m+j+1)*input_dur)
-        inp[j, idx] = spike_train
-
-        # store target output
-        idx = np.arange(store_start+(i*m+j)*store_dur, store_start+(i*m+j+1)*store_dur)
-        targets[j, idx] = 1.0
+# convolve input spikes with synaptic alpha kernel
+kernel = alpha(np.arange(0, 200.0/dt), tau=5.0/dt)
+kernel /= np.max(kernel)
+for c in range(m):
+    inp[:, c] = convolve1d(inp[:, c], weights=kernel)
 
 import matplotlib.pyplot as plt
-plt.imshow(inp, aspect='auto', interpolation='none')
+plt.imshow(inp.T, aspect='auto', interpolation='none')
 plt.colorbar()
 plt.show()
 
@@ -78,13 +143,15 @@ plt.show()
 data = {}
 data['T'] = T
 data['dt'] = dt
-data['dts'] = dts
+data['sr'] = sr
 data['cutoff'] = cutoff
 data['N'] = N
 data['p'] = p
-data['C'] = C
+data['W'] = W
 data['W_in'] = W_in
 data['inp'] = inp
-data['targets'] = targets.T
-data['number_input_channels'] = m
-pickle.dump(data, open(fname, 'wb'))
+data['stim_times'] = stim_times
+data['stim_channels'] = stim_channels
+data['node_vars'] = node_vars
+data['additional_params'] = {"v_reset": v_reset, "v_spike": v_spike}
+pickle.dump(data, open(f"config/{fname}_config.pkl", 'wb'))
--- reservoir_computing/simulation_rs_ir.py
+++ reservoir_computing/simulation_rs_ir.py
@@ -20,116 +20,51 @@ def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
 # model definition
 ##################
 
-# file name for saving
-fname = "rs_ir_1"
+# file name for loading/saving
+fname = "rs_ir"
 
-# network parameters
-N = 1000
-p = 0.05
-C = 100.0
-k = 0.7
-v_r = -60.0
-v_t = -40.0
-Delta = 1.0
-eta = 40.0
-a = 0.03
-b = -2.0
-d = 130.0
-g = 8.0
-E_r = 0.0
-tau_s = 6.0
-v_spike = 1000.0
-v_reset = -1000.0
+# load config
+config = pickle.load(open(f"config/{fname}_config.pkl", "rb"))
 
-# collect remaining model parameters
-node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": [], "eta": eta, "tau_u": 1/a, "b": b, "kappa": d, "g": g,
-             "E_r": E_r, "tau_s": tau_s, "v": v_t}
+# load model/simulation variables
+node_vars = config["node_vars"]
+I_ext = config["inp"]
+W = config["W"]
+W_in = config["W_in"]
+params = config["additional_params"]
+dt = config["dt"]
+sr = config["sr"]
 
-# simulation settings
-T = 21000.0
-dt = 1e-1
-steps = int(T/dt)
-sampling_steps = 10
-
-# input definition
-p_in = 0.2
-alpha = 300.0
-kappa = 200
-sigma = 20
-mean_isi = 20000
-stimuli = np.zeros((steps, 1))
-idx = 0
-stim_times = []
-while idx < steps:
-    if idx > 0:
-        stim_times.append(int(idx/sampling_steps))
-        stimuli[idx, 0] = 1.0
-    idx += mean_isi + int(np.random.randn() * kappa)
-
-# generate input
-I_ext = np.zeros_like(stimuli)
-I_ext[:, 0] = gaussian_filter1d(input=stimuli[:, 0], sigma=sigma)
-I_ext[:, 0] /= np.max(I_ext[:, 0])
-
-# parameter sweep definition
-params = ["p"]
-values = [[0.03125], [0.0625], [0.125], [0.25], [0.5], [1.0]]
+# run-specific parameters (for sweeps)
+alpha = 10.0
 
 # simulation
 ############
 
-results = []
-correlations = []
-thetas = []
-W_ins = []
-Js = []
-for vs in values:
+# initialize model
+net = Network.from_yaml("neuron_model_templates.spiking_neurons.ik.ik", weights=W, source_var="s", target_var="s_in",
+                        input_var="I_ext", output_var="s", spike_var="spike", spike_def="v",
+                        node_vars=node_vars.copy(), op="ik_op", spike_reset=params["v_reset"],
+                        spike_threshold=params["v_spike"], dt=dt, device="cpu")
+net.add_input_layer(W_in.shape[1], W_in, trainable=False)
 
-    # change parameters
-    for param, v in zip(params, vs):
-        if param in node_vars:
-            node_vars[param] = v
-        else:
-            exec(f"{param} = {v}")
-
-    # draw random variables
-    J = random_connectivity(N, N, p, normalize=True)
-    W_in = input_connections(N, stimuli.shape[1], p_in, variance=np.sqrt(1/p_in), zero_mean=True)
-    node_vars["v_theta"] = lorentzian(N, v_t, Delta, v_r, 2 * v_t - v_r)
-
-    # initialize model
-    net = Network.from_yaml("neuron_model_templates.spiking_neurons.ik.ik", weights=J, source_var="s", target_var="s_in",
-                            input_var="I_ext", output_var="s", spike_var="spike", spike_def="v",
-                            node_vars=node_vars.copy(), op="ik_op", spike_reset=v_reset, spike_threshold=v_spike, dt=dt,
-                            device="cuda:0")
-    net.add_input_layer(stimuli.shape[1], W_in, trainable=False)
-
-    # simulation
-    obs = net.run(inputs=I_ext * alpha, sampling_steps=sampling_steps, record_output=True)
-    results.append(obs["out"])
-
-    # results storage
-    projections = np.sum(J > 0, axis=0)
-    correlations.append(np.corrcoef(W_in[:, 0], projections))
-    Js.append(J)
-    W_ins.append(W_in)
-    thetas.append(node_vars["v_theta"])
+# simulation
+obs = net.run(inputs=I_ext * alpha, sampling_steps=sr, record_output=True)
 
 # save results
-inp = pd.DataFrame(index=results[-1].index, data=I_ext[::sampling_steps, :], columns=np.arange(0, stimuli.shape[1]))
-pickle.dump({"s": results, "J": Js, "heterogeneity": thetas, "stimuli": stim_times, "W_in": W_ins, "params": node_vars,
-             "T": T, "dt": dt, "sr": sampling_steps, "v_reset": v_reset, "v_spike": v_spike, "sweep": (params, values)},
-            open(f"results/{fname}.pkl", "wb"))
+res = obs["out"]
+inp = pd.DataFrame(index=res.index, data=I_ext[::sr, :], columns=np.arange(0, W_in.shape[1]))
+pickle.dump({"s": res, "I_ext": inp, "alpha": alpha},
+            open(f"results/{fname}_results.pkl", "wb"))
 
 # exemplary plotting
-for vs, s, c in zip(values, results, correlations):
-    _, ax = plt.subplots()
-    ax.plot(s.mean(axis=1), color="blue")
-    ax2 = ax.twinx()
-    ax2.plot(inp.iloc[:, 0], color="orange")
-    ax.set_xlabel("time (ms)")
-    ax.set_ylabel("s")
-    ax2.set_ylabel("I")
-    condition = ", ".join([f"{param} = {v}" for param, v in zip(params, vs)])
-    plt.title(f"{condition} : corr = {c[0, 1]}")
-    plt.show()
+_, axes = plt.subplots(nrows=2)
+ax = axes[0]
+ax.plot(res.mean(axis=1), color="blue")
+ax.set_xlabel("time (ms)")
+ax.set_ylabel("s")
+ax2 = axes[1]
+ax2.plot(inp.iloc[:, 0], color="orange")
+ax2.set_xlabel("time (ms)")
+ax2.set_ylabel("I")
+plt.show()
