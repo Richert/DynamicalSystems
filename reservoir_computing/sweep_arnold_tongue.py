9d1fb57135954c84a2e0a4e96eeaccd568dc113d
Richert <richgast.90@gmail.com>
2023-02-10 16:04:38 -0600

    nwe reservoir computing stuff

---
+++ reservoir_computing/sweep_arnold_tongue.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pickle
+
+# parameter sweep definition
+sweep = dict()
+sweep["alpha"] = np.linspace(0.0, 0.001, num=20)
+sweep["p_in"] = np.linspace(0.0, 1.0, num=10)
+
+# save sweep to file
+fname = "arnold_tongue_sweep"
+with open(f"config/{fname}.pkl", "wb") as f:
+    pickle.dump(sweep, f)
+    f.close()
--- reservoir_computing/worker_arnold_tongue.py
+++ reservoir_computing/worker_arnold_tongue.py
@@ -1,70 +1,135 @@
+from rectipy import Network, random_connectivity, circular_connectivity
 import numpy as np
-from pyrates import CircuitTemplate, NodeTemplate, grid_search
 import pickle
+from scipy.stats import cauchy, rv_discrete
 import sys
 
 
-# network definition
-####################
+def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = cauchy.rvs(loc=eta, scale=delta)
+        while s <= lb or s >= ub:
+            s = cauchy.rvs(loc=eta, scale=delta)
+        samples[i] = s
+    return samples
 
-# define network nodes
-ko = NodeTemplate.from_yaml("model_templates.oscillators.kuramoto.sin_pop")
-ik = NodeTemplate.from_yaml("model_templates.neural_mass_models.ik.ik_theta_pop")
-nodes = {'ik': ik, 'ko': ko}
 
-# define network edges
-edges = [
-    ('ko/sin_op/s', 'ik/ik_theta_op/r_in', None, {'weight': 0.001}),
-    ('ik/ik_theta_op/r', 'ik/ik_theta_op/r_in', None, {'weight': 1.0})
-]
+def dist(x: int, method: str = "inverse") -> float:
+    if method == "inverse":
+        return 1/x if x > 0 else 1
+    if method == "inverse_squared":
+        return 1/x**2 if x > 0 else 1
+    if method == "exp":
+        return np.exp(-x)
+    else:
+        raise ValueError("Invalid method.")
 
-# initialize network
-net = CircuitTemplate(name="ik_forced", nodes=nodes, edges=edges)
 
-# update izhikevich parameters
-node_vars = {
-    "C": 100.0,
-    "k": 0.7,
-    "v_r": -60.0,
-    "v_t": -40.0,
-    #"eta": 55.0,
-    "Delta": 0.1,
-    "g": 15.0,
-    "E_r": 0.0,
-    "b": -2.0,
-    "a": 0.03,
-    "d": 100.0,
-    "tau_s": 6.0,
-}
-node, op = "ik", "ik_theta_op"
-net.update_var(node_vars={f"{node}/{op}/{var}": val for var, val in node_vars.items()})
+# model definition
+##################
 
-# perform parameter sweep
-#########################
+# sweep condition
+cond = int(sys.argv[-1])
+p1 = "p_in"
+p2 = "alpha"
 
-# define sweep
-alphas = np.linspace(1, 10, num=10)*1e-4
-omegas = np.linspace(1, 6, num=10)*1e-3
-sweep = {"alpha": alphas, "omega": omegas}
-param_map = {"alpha": {"vars": ["weight"], "edges": [('ko/sin_op/s', 'ik/ik_theta_op/r_in')]},
-             "omega": {"vars": ["phase_op/omega"], "nodes": ["ko"]}}
+# network parameters
+N = 1000
+p = 0.1
+C = 100.0
+k = 0.7
+v_r = -60.0
+v_t = -40.0
+Delta = 1.0
+eta = 55.0
+a = 0.03
+b = -2.0
+d = 100.0
+g = 15.0
+E_r = 0.0
+tau_s = 6.0
+v_spike = 1000.0
+v_reset = -1000.0
+
+# parameter sweep definition
+with open("/home/rgf3807/PycharmProjects/DynamicalSystems/reservoir_computing/config/arnold_tongue_sweep.pkl", "rb") as f:
+    sweep = pickle.load(f)
+    v1s = sweep[p1]
+    v2s = sweep[p2]
+    f.close()
+vals = [(v1, v2) for v1 in v1s for v2 in v2s]
+v1, v2 = vals[cond]
 
 # simulation parameters
 cutoff = 30000.0
 T = 300000.0 + cutoff
 dt = 1e-2
-dts = 1e-1
-inp = np.zeros((int(T/dt),)) + 55.0
+sr = 100.0
+steps = int(np.round(T/dt))
+time = np.linspace(0.0, T, num=steps)
 
-# perform sweep
-res, res_map = grid_search(net, param_grid=sweep, param_map=param_map, simulation_time=T, step_size=dt,
-                           sampling_step_size=dts, cutoff=cutoff, permute_grid=True, vectorize=True,
-                           inputs={"ik/ik_theta_op/I_ext": inp},
-                           outputs={"ik": "ik/ik_theta_op/r", "ko": "ko/phase_op/theta"},
-                           solver="euler", float_precision="float64"
-                           )
+# input definition
+omega = 0.005
+alpha = 1.0
+p_in = 0.1
+I_ext = np.zeros((steps, 1))
+I_ext[:, 0] = np.sin(2.0*np.pi*omega*time)
 
-# save data
-fn = sys.argv[-1]
-pickle.dump({"res": res, "map": res_map, "alphas": alphas, "omegas": omegas},
-            open(f"{fn}_hom.pkl", "wb"))
+# simulation
+############
+
+results = {"s": [], "J": [], "thetas": [], "W_in": [], "sweep": {p1: v1, p2: v2}, "T": T, "dt": dt,
+           "sr": sr}
+n_reps = 10
+for idx in range(n_reps):
+
+    # adjust parameters according to sweep condition
+    for param, v in zip([p1, p2], [v1, v2]):
+        exec(f"{param} = {v}")
+
+    # create connectivity matrix
+    connectivity = "exponential"
+    indices = np.arange(0, N, dtype=np.int32)
+    pdfs = np.asarray([dist(idx, method="inverse_squared") for idx in indices])
+    pdfs /= np.sum(pdfs)
+    if connectivity == "circular":
+        W = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
+    else:
+        W = random_connectivity(N, N, p, normalize=True)
+
+    # create input matrix
+    W_in = np.zeros((N, 1))
+    idx = np.random.choice(np.arange(N), size=int(N*p_in), replace=False)
+    W_in[idx, 0] = alpha
+
+    # create background current distribution
+    thetas = lorentzian(N, v_t, Delta, v_r, 2 * v_t - v_r)
+
+    # collect remaining model parameters
+    node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": thetas, "eta": eta, "tau_u": 1/a, "b": b, "kappa": d, "g": g,
+                 "E_r": E_r, "tau_s": tau_s, "v": v_t}
+
+    # initialize model
+    net = Network.from_yaml("config/ik/rs", weights=W, source_var="s", target_var="s_in",
+                            input_var="I_ext", output_var="s", spike_var="spike", spike_def="v", to_file=False,
+                            node_vars=node_vars.copy(), op="rs_op", spike_reset=v_reset, spike_threshold=v_spike, dt=dt,
+                            verbose=False, clear=True)
+    net.add_input_layer(1, weights=W_in)
+
+    # simulation
+    obs = net.run(inputs=I_ext, sampling_steps=sr, record_output=True, verbose=False)
+
+    # results storage
+    results["s"].append(obs["out"])
+    results["J"].append(W)
+    results["thetas"].append(thetas)
+    results["W_in"].append(W_in)
+
+    print(f"Run {idx} done for condition {cond}.")
+
+# save results
+fname = f"rs_arnold_tongue"
+with open(f"/projects/p31302/richard/results/{fname}_{cond}.pkl", "wb") as f:
+    pickle.dump(results, f)
+    f.close()
