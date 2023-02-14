e27f2556842916123f2e7c1c3f7eab15b05e5d86
Richert <richgast.90@gmail.com>
2023-02-14 16:03:10 -0600

    nwe reservoir computing stuff

--- reservoir_computing/entrainment_mean_field.py
+++ reservoir_computing/entrainment_mean_field.py
@@ -39,14 +39,14 @@ fs = int(np.round(1000.0/(res.index[1] - res.index[0]), decimals=0))
 
 # filtering options
 print(f"Sampling frequency: {fs}")
-f_margin = 0.5
+f_margin = 1.0
 print(f"Frequency band width (Hz): {2*f_margin}")
 f_order = 32
 f_cutoff = 250000
 
 # Plot the frequency response for a few different orders.
 plt.figure(1)
-for order, omega in zip([32, 64, 32, 64], [2.0, 2.0, 4.0, 4.0]):
+for order, omega in zip([16, 32, 16, 32], [2.0, 2.0, 4.0, 4.0]):
     sos = butter(order, [omega-f_margin, omega+f_margin], fs=fs, btype='band', output='sos')
     w, h = sosfreqz(sos, worN=12000)
     plt.plot((fs * 0.5 / np.pi) * w, abs(h), label=f"order = {order}, omega = {omega}")
@@ -74,7 +74,6 @@ for key in res_map.index:
 
     # filter data around driving frequency
     ik_filtered = butter_bandpass_filter(ik, omega-f_margin*omega, omega+f_margin*omega, fs=fs, order=f_order)
-    ik_filtered /= np.max(ik_filtered)
 
     # get analytic signals
     ik_phase, ik_env = analytic_signal(ik_filtered)
