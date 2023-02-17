f05c89cb8fa1da98e2d4342431d175e916f66d76
Richert <richgast.90@gmail.com>
2023-02-17 13:55:35 -0600

    updates worker script for the arnold tongue simulations

---
+++ reservoir_computing/cleanup_arnold_tongue.py
@@ -0,0 +1,30 @@
+import pickle
+import numpy as np
+import sys
+
+# load data
+fn = sys.argv[-1] #"results/rs_arnold_tongue_hom.pkl"
+data = pickle.load(open(fn, "rb"))
+
+# input definition
+cutoff = 20000.0
+T = 200000.0 + cutoff
+dt = 1e-2
+sr = 100
+steps = int(np.round(T/dt))
+time = np.linspace(0.0, T, num=steps)
+omega = 0.005
+I_ext = np.zeros((steps, 1))
+I_ext[:, 0] = np.sin(2.0*np.pi*omega*time)
+
+# check whether crucial info exists in results file. if not, add it to the file.
+check_keys = ["omega", "p", "Delta", "I_ext"]
+check_vals = [omega, 0.1, 1.0, I_ext[::sr]]
+for key, val in zip(check_keys, check_vals):
+    if key not in data:
+        data[key] = val
+
+# save data
+with open(fn, "wb") as f:
+    pickle.dump(data, f)
+    f.close()
