d49e8a925879560ee112229ff03c8109d222f64a
Richert <richgast.90@gmail.com>
2023-03-04 16:18:54 -0600

    updates worker script for the arnold tongue simulations

--- reservoir_computing/simulation_multi_pop.py
+++ reservoir_computing/simulation_multi_pop.py
@@ -3,15 +3,35 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from scipy.optimize import minimize_scalar
+from scipy.stats import norm, cauchy
 
 
-def fit_lorentzian(x: np.ndarray, modules: dict, nodes: list) -> np.ndarray:
+def fit_lorentzian(x: np.ndarray, modules: dict, nodes: list, delta_lb: float = 0.01, delta_ub: float = 10.0,
+                   x_range: float = 1.5) -> tuple:
     deltas = []
+    mus = []
     x = x[nodes]
-    for _, (indices, ) in modules.items():
+    for indices, _ in modules.values():
         x_mod = x[indices]
-        deltas.append(np.std(x_mod))
-    return np.asarray(deltas)
+        x_mean = np.mean(x_mod)
+        x_lb = np.min(x_mod)*x_range
+        x_ub = np.max(x_mod)*x_range
+        res = minimize_scalar(sample_sd_cauchy, bounds=(delta_lb, delta_ub), args=(x_mod, x_mean, len(x_mod), x_lb, x_ub),
+                              method='bounded', options={'maxiter': 1000, 'disp': True})
+        deltas.append(res.x)
+        mus.append(x_mean)
+    return np.asarray(deltas), np.asarray(mus)
+
+
+def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = cauchy.rvs(loc=eta, scale=delta)
+        while s <= lb or s >= ub:
+            s = cauchy.rvs(loc=eta, scale=delta)
+        samples[i] = s
+    return samples
 
 
 def get_module_coupling(W: np.ndarray, modules: dict, nodes: list) -> np.ndarray:
@@ -21,18 +41,32 @@ def get_module_coupling(W: np.ndarray, modules: dict, nodes: list) -> np.ndarray
     for i, mod1 in enumerate(modules):
         targets, _ = modules[mod1]
         for j, mod2 in enumerate(modules):
-            sources, _ = modules[mod1]
+            sources, _ = modules[mod2]
             W_tmp = W[targets, :]
             W_tmp = W_tmp[:, sources]
-            W_mod[i, j] = np.mean(W_tmp > 0.0)
+            W_mod[i, j] = np.mean(np.sum(W_tmp, axis=1))
     return W_mod
 
+
+def gaussian(n, mu: float, sd: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = norm.rvs(loc=mu, scale=sd)
+        while s <= lb or s >= ub:
+            s = norm.rvs(loc=mu, scale=sd)
+        samples[i] = s
+    return samples
+
+
+def sample_sd_cauchy(delta: float, samples: np.ndarray, mu: float, n_samples: int, lb: float, ub: float):
+    samples_c = lorentzian(n_samples, eta=mu, delta=delta, lb=lb, ub=ub)
+    return (np.std(samples_c) - np.std(samples))**2
 
 # load SNN data
 module_examples = {"lc": [], "ss": []}
 example = 0
 condition = {"Delta": 1.0, "p": 0.03125}
-path = "results/dimensionality2"
+path = "results/dimensionality"
 fn = "rs_dimensionality"
 for file in os.listdir(path):
     if fn in file:
@@ -51,7 +85,7 @@ for file in os.listdir(path):
             if include_example:
                 key = "ss" if dim_tmp["d"] < 50.0 else "lc"
                 module_examples[key].append({"m": mods["m"][i], "s": mods["s"][i], "cov": mods["cov"][i],
-                                             "W": mods["W"][i], "nodes": mods[i]["nodes"], "thetas": mods[i]["thetas"]}
+                                             "W": mods["W"][i], "nodes": np.arange(0, mods["cov"][i].shape[0]), "thetas": lorentzian(1000, -40.0, Delta, -60.0, -20.0)}  #nodes: mods[i]["nodes"], thetas: mods[i]["thetas"]
                                             )
                 example += 1
 
@@ -65,32 +99,47 @@ modules = example["m"]
 nodes = example["nodes"]
 
 # fit theta distribution for each module
-deltas = fit_lorentzian(thetas, modules, nodes)
+deltas, thresholds = fit_lorentzian(thetas, modules, nodes, x_range=1.0)
 
 # approximate the connectivity within and between modules
 W_mods = get_module_coupling(W, modules, nodes)
 
 # create mean-field model
-rs = NodeTemplate.from_yaml("results/ik/rs_mf")
-mf = CircuitTemplate("rs", nodes={key: rs for key in modules})
-mf.add_edges_from_matrix("p/rs_mf_op/s", "p/rs_mf_op/s_in", nodes=list(modules.keys()), weight=W_mods)
+C = 100.0
+k = 0.7
+v_r = -60.0
+v_t = -40.0
+eta = 55.0
+a = 0.03
+b = -2.0
+g = 15.0
+E_r = 0.0
+tau_s = 6.0
+node_vars = {"C": C, "k": k, "v_r": v_r, "v_t": thresholds, "Delta": deltas, "tau_u": 1/a, "b": b, "E_r": E_r,
+             "eta": eta, "kappa": 10.0 if condition == "ss" else 100.0, "g": g, "tau_s": tau_s, "v": v_t}
+rs = NodeTemplate.from_yaml("config/ik/rs_mf")
+nodes = [str(key) for key in modules]
+mf = CircuitTemplate("rs", nodes={key: rs for key in nodes})
+mf.add_edges_from_matrix("rs_mf_op/s", "rs_mf_op/s_in", nodes=nodes, weight=W_mods)
+mf.update_var(node_vars={f"all/rs_mf_op/{key}": val for key, val in node_vars.items()})
 
 # simulate mean-field model dynamics
 cutoff = 2000.0
 T = 3000.0 + cutoff
 dt = 1e-3
 sr = 1000
-res = mf.run(simulation_time=T, step_size=dt, sampling_step_size=dt*sr, outputs={"s": "p/rs_mf_op/s"})
+res = mf.run(simulation_time=T, step_size=dt, sampling_step_size=dt*sr, outputs={"s": "all/rs_mf_op/s"},
+             solver="scipy", method="DOP853", cutoff=cutoff)
 
 # plotting
-fig, axes = plt.subplots(nrows=len(modules), figsize=(10, len(modules)))
+fig, axes = plt.subplots(nrows=len(modules), figsize=(10, 2*len(modules)))
 for i, mod in enumerate(modules):
     ax = axes[i]
     ax.plot(res.index, example["s"][mod], label="SNN")
-    ax.plot(res.index, res["s"][mod], label="MF")
+    ax.plot(res.index, res["s"][str(mod)], label="MF")
     ax.set_xlabel("time (ms)")
     ax.set_ylabel("s")
     ax.set_title(f"Module {mod}")
-    plt.legend()
+    ax.legend()
 plt.tight_layout()
 plt.show()
