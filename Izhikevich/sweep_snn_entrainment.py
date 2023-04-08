2b2821c1de365b34a3e0e0e7447b811426f24481
Richert <richgast.90@gmail.com>
2023-04-08 17:16:01 -0500

    updates worker script for the arnold tongue simulations

--- Izhikevich/sweep_snn_entrainment.py
+++ Izhikevich/sweep_snn_entrainment.py
@@ -3,7 +3,7 @@ import pickle
 
 # parameter sweep definition
 sweep = dict()
-sweep["Delta"] = np.linspace(0.01, 2.0, num=20)
+sweep["Delta"] = np.linspace(0.01, 1.6, num=20)
 sweep["trial"] = np.arange(0, 10)
 
 # load data that relate Delta to omega
