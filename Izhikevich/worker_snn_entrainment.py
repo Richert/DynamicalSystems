e56ab66f42e92d0e954919ebc7874639d575109b
Richert <richgast.90@gmail.com>
2023-04-08 17:15:17 -0500

    updates worker script for the arnold tongue simulations

--- Izhikevich/worker_snn_entrainment.py
+++ Izhikevich/worker_snn_entrainment.py
@@ -1,7 +1,7 @@
 from rectipy import Network, circular_connectivity
 import sys
-cond, wdir, tdir = sys.argv[-3:]
-sys.path.append(wdir)
+# cond, wdir, tdir = sys.argv[-3:]
+# sys.path.append(wdir)
 sys.path.append("~/PycharmProjects/DynamicalSystems/reservoir_computing")
 import numpy as np
 from scipy.stats import cauchy
@@ -95,11 +95,11 @@ v_reset = -1000.0
 device = "cuda:0"
 
 # working directory
-# wdir = "config"
-# tdir = "results"
+wdir = "config"
+tdir = "results"
 
 # sweep condition
-# cond = 10
+cond = 10
 p1 = "Delta"
 p2 = "trial"
 
@@ -172,7 +172,7 @@ for conn_pow in conn_pows:
     pdfs /= np.sum(pdfs)
     W = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)), homogeneous_weights=False)
 
-    # initialize model
+    # model parameters
     node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": thetas, "eta": eta, "tau_u": 1 / a, "b": b, "kappa": d,
                  "g": g, "E_r": E_r, "tau_s": tau_s, "v": v_t}
 
@@ -189,8 +189,8 @@ for conn_pow in conn_pows:
 
     # perform simulation
     obs = net.run(inputs=inp, sampling_steps=sr, record_output=True, verbose=False)
-    res = obs["out"].iloc[cutoff:, :].values
-    s = gaussian_filter1d(res, sigma=sigma, axis=0).T
+    res = obs["out"]
+    s = gaussian_filter1d(res.iloc[cutoff:, :].values, sigma=sigma, axis=0).T
 
     # calculate the correlation between the network
     snn_driven = np.mean(s[inp_indices, :], axis=0)
@@ -216,9 +216,6 @@ for conn_pow in conn_pows:
 
     # detect neural oscillations
     s_smoothed = gaussian_filter1d(res.values, sigma=20 * sigma, axis=0).T
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.imshow(s_smoothed, aspect="auto", interpolation="none")
-    plt.show()
     neural_freqs = []
     for idx in range(s_smoothed.shape[0]):
         s_tmp = s_smoothed[idx, :]
@@ -262,12 +259,12 @@ for conn_pow in conn_pows:
     # ax.set_ylabel("s")
     # ax.set_title(f"Mean activity (corr_driven = {corr_driven}, corr_nondriven = {corr_nondriven})")
     # ax = axes[3]
-    # ax.plot(oscillatory_spiking)
+    # ax.plot(neural_freqs)
     # ax.axvline(x=inp_indices[0], linestyle="--", color="blue")
     # ax.axvline(x=inp_indices[-1], linestyle="--", color="blue")
     # ax.set_xlabel("neuron id")
-    # ax.set_ylabel("Low-freq strength")
-    # ax.set_title(f"Relative amount of long ISIs")
+    # ax.set_ylabel("freq")
+    # ax.set_title(f"Neural oscillation freq")
     # plt.tight_layout()
     # plt.show()
 
