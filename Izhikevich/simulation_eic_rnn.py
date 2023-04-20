c807cc6bbfefea8963cc5bb21b119b1a60583580
Richert <richgast.90@gmail.com>
2023-04-20 09:00:45 -0500

    nwe reservoir computing stuff

--- Izhikevich/simulation_eic_rnn.py
+++ Izhikevich/simulation_eic_rnn.py
@@ -1,119 +1,42 @@
-import numba as nb
-nb.config.THREADING_LAYER = 'omp'
-nb.set_num_threads(4)
+from rectipy import Network, circular_connectivity
+from pyrates import CircuitTemplate, NodeTemplate
+import sys
+sys.path.append("~/PycharmProjects/DynamicalSystems/reservoir_computing")
 import numpy as np
-from pyrecu import RNN
+from reservoir_computing.utility_funcs import lorentzian, dist
 import matplotlib.pyplot as plt
 import pickle
-from scipy.stats import cauchy
-plt.rcParams['backend'] = 'TkAgg'
-
-
-def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
-    samples = np.zeros((n,))
-    for i in range(n):
-        s = cauchy.rvs(loc=eta, scale=delta)
-        while s <= lb or s >= ub:
-            s = cauchy.rvs(loc=eta, scale=delta)
-        samples[i] = s
-    return samples
-
-
-def ik_ei_ata(y: np.ndarray, N: int, inp: np.ndarray, ve_r: float, vi_r: float, ve_t: np.ndarray, vi_t: np.ndarray,
-              ke: float, ki: float, Ce: float, Ci: float, ae: float, ai:float, be: float, bi: float, de: float,
-              di:float, ve_spike: float, vi_spike: float, ve_reset: float, vi_reset: float, g_ampa: float,
-              g_gaba: float, E_ampa: float, E_gaba: float, tau_ampa: float, tau_gaba: float, k_ee: float, k_ei: float,
-              k_ie: float, k_ii: float, dt: float = 1e-4) -> np.ndarray:
-    """Calculates right-hand side update of a network of all-to-all coupled Izhikevich neurons of the biophysical form
-     with heterogeneous background excitabilities, split into an excitatory and an inhibitory population.
-     """
-
-    # preparatory calculations
-    ##########################
-
-    # extract state variables from u
-    m = 4*N
-    ve, ue, vi, ui = y[:N], y[N:2*N], y[2*N:3*N], y[3*N:m]
-    se_ampa, se_gaba, si_ampa, si_gaba = y[m], y[m+1], y[m+2], y[m+3]
-
-    # extract inputs
-    inp_e, inp_i = inp[0], inp[1]
-
-    # calculate network firing rates
-    spikes_e = ve >= ve_spike
-    rates_e = np.mean(spikes_e / dt)
-    spikes_i = vi >= vi_spike
-    rates_i = np.mean(spikes_i / dt)
-
-    # calculate vector field of the system
-    ######################################
-
-    dy1 = [
-        # excitatory population
-        (ke*(ve**2 - (ve_r+ve_t)*ve + ve_r*ve_t) + inp_e + g_ampa*se_ampa*(E_ampa-ve) + g_gaba*se_gaba*(E_gaba-ve) - ue) / Ce,
-        ae*(be*(ve-ve_r) - ue),
-
-        # inhibitory population
-        (ki * (vi**2 - (vi_r+vi_t)*vi + vi_r*vi_t) + inp_i + g_ampa*si_ampa*(E_ampa-vi) + g_gaba*si_gaba*(E_gaba-vi) - ui) / Ci,
-        ai*(bi*(vi-vi_r) - ui),
-    ]
-    dy2 = [
-        # synapses
-        k_ee*rates_e - se_ampa / tau_ampa,
-        k_ei*rates_i - se_gaba / tau_gaba,
-        k_ie*rates_e - si_ampa / tau_ampa,
-        k_ii*rates_i - si_gaba / tau_gaba,
-    ]
-
-    # update state variables
-    ########################
-
-    # euler integration
-    for i in nb.prange(len(dy1)):
-        y[i*N:(i+1)*N] += dt * dy1[i]
-    for j in nb.prange(len(dy2)):
-        y[m+j] += dt * dy2[j]
-
-    # reset membrane potential and apply spike frequency adaptation
-    y[:N][spikes_e] = ve_reset
-    y[N:2*N][spikes_e] += de
-    y[2*N:3*N][spikes_i] = vi_reset
-    y[3*N:m][spikes_i] += di
-
-    return y
+from scipy.stats import rv_discrete
 
 
 # define parameters
 ###################
 
-N = 10000
-
-# define model parameters
-#########################
+# general parameters
+N = 1000
+p = 0.2
+v_spike = 1e3
+v_reset = -1e3
 
 # RS neuron parameters
 Ce = 100.0   # unit: pF
 ke = 0.7  # unit: None
 ve_r = -60.0  # unit: mV
 ve_t = -40.0  # unit: mV
-ve_spike = 40.0  # unit: mV
-ve_reset = -60.0  # unit: mV
 Delta_e = 1.0  # unit: mV
-de = 0.0
+de = 20.0
 ae = 0.03
-be = 0.0
+be = -2.0
 
 # IB neuron parameters
 Ci = 20.0   # unit: pF
 ki = 1.0  # unit: None
 vi_r = -55.0  # unit: mV
 vi_t = -40.0  # unit: mV
-vi_spike = 40.0  # unit: mV
-vi_reset = -60.0  # unit: mV
-Delta_i = 1.5  # unit: mV
+Delta_i = 0.3  # unit: mV
 di = 0.0
 ai = 0.2
-bi = 0.0
+bi = 0.025
 
 # synaptic parameters
 g_ampa = 1.0
@@ -131,43 +54,69 @@ k_ii = 4.0
 spike_thresholds_e = lorentzian(N, eta=ve_t, delta=Delta_e, lb=ve_r, ub=ve_r-ve_t)
 spike_thresholds_i = lorentzian(N, eta=vi_t, delta=Delta_i, lb=vi_r, ub=vi_r-vi_t)
 
+# define connectivity
+indices = np.arange(0, N, dtype=np.int32)
+pdfs = np.asarray([dist(idx, method="inverse", zero_val=0.0, inverse_pow=1.5) for idx in indices])
+pdfs /= np.sum(pdfs)
+W_ee = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
+W_ie = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
+W_ei = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
+W_ii = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, pdfs)))
+
 # define inputs
-T = 4000.0
-cutoff = 1000.0
+T = 2500.0
+cutoff = 500.0
 dt = 1e-3
 dts = 1e-1
-I_ext = np.zeros((int(T/dt), 2))
-I_ext[:, 0] += 50.0
-I_ext[:, 1] += 20.0
-I_ext[int(2000/dt):int(3000/dt), 1] -= 20.0
+inp = np.zeros((int(T/dt), 1)) + 25.0
+inp[int(1000/dt):int(2000/dt), 0] += 25.0
 
 # run the model
 ###############
 
-# initialize model
-u_init = np.zeros((4*N+4,))
-u_init[:N] -= 60.0
-u_init[2*N:3*N] -= 60.0
-model = RNN(N, 4*N+4, ik_ei_ata, Ce=Ce, Ci=Ci, ke=ke, ki=ki, ve_r=ve_r, vi_r=vi_r, ve_t=spike_thresholds_e,
-            vi_t=spike_thresholds_i, ve_spike=ve_spike, vi_spike=vi_spike, ve_reset=ve_reset, vi_reset=vi_reset,
-            de=de, di=di, ae=ae, ai=ai, be=be, bi=bi, g_ampa=g_ampa, g_gaba=g_gaba, E_ampa=E_ampa, E_gaba=E_gaba,
-            tau_ampa=tau_ampa, tau_gaba=tau_gaba, k_ee=k_ee, k_ei=k_ei, k_ie=k_ie, k_ii=k_ii, u_init=u_init)
+# initialize nodes
+e_vars = {"C": Ce, "k": ke, "v_r": ve_r, "v_theta": spike_thresholds_e, "eta": 0.0, "tau_u": 1/ae, "b": be, "kappa": de,
+          "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_ampa, "v": ve_t}
+i_vars = {"C": Ci, "k": ki, "v_r": vi_r, "v_theta": spike_thresholds_i, "eta": 0.0, "tau_u": 1/ai, "b": bi, "kappa": di,
+          "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_gaba, "v": vi_t}
+rs = NodeTemplate.from_yaml("config/ik_snn/ik")
+fs = NodeTemplate.from_yaml("config/ik_snn/ik")
 
-# define outputs
-outputs = {'se': {'idx': np.asarray([4*N]), 'avg': False}, 'si': {'idx': np.asarray([4*N+1]), 'avg': False}}
+# construct rs and fs circuits
+rs_neurons = {f'rs_{i}': rs for i in range(N)}
+rs_net = CircuitTemplate("rs", nodes=rs_neurons)
+rs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ee,
+                             source_nodes=list(rs_neurons.keys()))
+rs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in e_vars.items()})
+fs_neurons = {f'fs_{i}': fs for i in range(N)}
+fs_net = CircuitTemplate("fs", nodes=fs_neurons)
+fs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ii,
+                             source_nodes=list(fs_neurons.keys()))
+fs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in i_vars.items()})
+
+# combine the rs and fs populations into a single circuit
+net = CircuitTemplate("eic", circuits={"rs": rs_net, "fs": fs_net})
+net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ie,
+                          source_nodes=[f"rs/rs_{i}" for i in range(N)], target_nodes=[f"fs/fs_{i}" for i in range(N)])
+net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ei,
+                          source_nodes=[f"fs/fs_{i}" for i in range(N)], target_nodes=[f"rs/rs_{i}" for i in range(N)])
+
+# initialize rectipy model
+model = Network(dt=dt, device="cpu")
+model.add_diffeq_node("eic", node=net, input_var="I_ext", output_var="s", spike_var="spike", spike_def="v",
+                      op="ik_op", spike_reset=v_reset, spike_threshold=v_spike, verbose=False, clear=True)
 
 # perform simulation
-res = model.run(T=T, dt=dt, dts=dts, outputs=outputs, inp=I_ext, cutoff=cutoff, parallel=True, fastmath=True)
+obs = model.run(inputs=inp, sampling_steps=int(dts/dt), record_output=True, verbose=False)
+res = obs.to_numpy("out")
 
 # plot results
-fig, ax = plt.subplots(figsize=(12, 6))
-ax.plot(res["se"]/k_ee)
-ax.plot(res["si"]/k_ei)
+fig, ax = plt.subplots(figsize=(12, 3))
+ax.plot(np.mean(res, axis=1))
 ax.set_ylabel(r'$s(t)$')
 ax.set_xlabel('time')
-plt.legend(['RS', 'FS'])
 plt.tight_layout()
 plt.show()
 
 # save results
-pickle.dump({'results': res}, open("results/eic_rnn_het.p", "wb"))
+pickle.dump({'results': res}, open("results/rs_snn_hom_low_sfa.p", "wb"))
