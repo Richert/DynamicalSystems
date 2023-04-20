8f53ac4159f2df93a480765498cb25bbea202b91
Richert <richgast.90@gmail.com>
2023-04-20 16:05:04 -0500

    updates worker script for the arnold tongue simulations

--- Izhikevich/bifurcation_eic_shadowing.py
+++ Izhikevich/bifurcation_eic_shadowing.py
@@ -15,8 +15,8 @@ path = sys.argv[-1]
 auto_dir = path if type(path) is str and ".py" not in path else "~/PycharmProjects/auto-07p"
 
 # config
-n_dim = 10
-n_params = 40
+n_dim = 8
+n_params = 33
 a = ODESystem("config", auto_dir=auto_dir, init_cont=False)
 
 # initial continuation in time (to converge to fixed point)
@@ -37,17 +37,17 @@ c1_sols, c1_cont = a.run(starting_point='UZ1', c='qif', ICP=15, NPAR=n_params, N
 
 # continuation in Delta_fs
 vals = [0.2, 2.0]
-c2_sols, c2_cont = a.run(starting_point='UZ1', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='D_fs:1',
-                         origin=c1_cont, NMX=8000, DSMAX=0.1, UZR={26: vals}, STOP=[], NPR=100, RL1=3.0,
+c2_sols, c2_cont = a.run(starting_point='UZ1', c='qif', ICP=24, NPAR=n_params, NDIM=n_dim, name='D_fs:1',
+                         origin=c1_cont, NMX=8000, DSMAX=0.1, UZR={24: vals}, STOP=[], NPR=100, RL1=3.0,
                          RL0=0.0, bidirectional=True)
 
-# continuation in d_rs
+# continuation in d_rs for low Delta_fs
 vals = [10.0, 100.0]
 c3_sols, c3_cont = a.run(starting_point='UZ1', c='qif', ICP=18, NPAR=n_params, NDIM=n_dim, name='d_rs:1',
                          origin=c2_cont, NMX=8000, DSMAX=0.1, UZR={18: vals}, STOP=[], NPR=100, RL1=150.0,
                          RL0=0.0, bidirectional=True)
 
-# continuation in d_rs
+# continuation in d_rs for high Delta_fs
 vals = [10.0, 100.0]
 c4_sols, c4_cont = a.run(starting_point='UZ2', c='qif', ICP=18, NPAR=n_params, NDIM=n_dim, name='d_rs:2',
                          origin=c2_cont, NMX=8000, DSMAX=0.1, UZR={18: vals}, STOP=[], NPR=100, RL1=150.0,
@@ -57,43 +57,43 @@ c4_sols, c4_cont = a.run(starting_point='UZ2', c='qif', ICP=18, NPAR=n_params, N
 ##############################################
 
 # continuation in I_fs for Delta_fs = 0.1 and d_rs = 10.0
-r1_sols, r1_cont = a.run(starting_point='UZ1', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:1',
+r1_sols, r1_cont = a.run(starting_point='UZ1', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:1',
                          origin=c3_cont, NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL1=150.0)
-a.run(starting_point='HB1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:1:hb1', origin=r1_cont,
-      NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
-a.run(starting_point='HB3', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:1:hb2', origin=r1_cont,
-      NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
+a.run(starting_point='HB1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:1:hb1', origin=r1_cont,
+      NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2', 'BP1'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
+# a.run(starting_point='HB3', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:1:hb2', origin=r1_cont,
+#       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
 
 # continuation in I_fs for Delta_fs = 0.1 and d_rs = 100.0
-r2_sols, r2_cont = a.run(starting_point='UZ2', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:2',
+r2_sols, r2_cont = a.run(starting_point='UZ2', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:2',
                          origin=c3_cont, NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL1=150.0)
-a.run(starting_point='HB1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:hb1', origin=r2_cont,
+a.run(starting_point='HB1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:hb1', origin=r2_cont,
       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
-a.run(starting_point='HB3', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:hb2', origin=r2_cont,
+a.run(starting_point='HB2', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:hb2', origin=r2_cont,
       NMX=8000, DSMAX=0.1, UZR={6: [0.02]}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
-a.run(starting_point='UZ1', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:2:2', origin='D_rs/I_fs:2:hb2',
+a.run(starting_point='UZ1', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:2:2', origin='D_rs/I_fs:2:hb2',
       NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL0=0.0, DS="-")
-a.run(starting_point='LP1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:lp1', origin='I_fs:2:2',
+a.run(starting_point='LP1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:2:lp1', origin='I_fs:2:2',
       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
 
-# continuation in I_fs for Delta_fs = 1.0 and d_rs = 10.0
-r3_sols, r3_cont = a.run(starting_point='UZ1', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:3',
+# continuation in I_fs for Delta_fs = 2.0 and d_rs = 10.0
+r3_sols, r3_cont = a.run(starting_point='UZ1', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:3',
                          origin=c4_cont, NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL1=150.0)
-a.run(starting_point='LP1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:3:lp1', origin=r3_cont,
+a.run(starting_point='LP1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:3:lp1', origin=r3_cont,
       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
-a.run(starting_point='LP2', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:3:lp2', origin=r3_cont,
+a.run(starting_point='LP2', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:3:lp2', origin=r3_cont,
       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
 
-# continuation in I_fs for Delta_fs = 1.0 and d_rs = 100.0
-r4_sols, r4_cont = a.run(starting_point='UZ2', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:4',
+# continuation in I_fs for Delta_fs = 2.0 and d_rs = 100.0
+r4_sols, r4_cont = a.run(starting_point='UZ2', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:4',
                          origin=c4_cont, NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL1=200.0)
-a.run(starting_point='HB1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:4:hb1', origin=r4_cont,
-      NMX=8000, DSMAX=0.1, UZR={30: [28.0]}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
+a.run(starting_point='HB1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:4:hb1', origin=r4_cont,
+      NMX=8000, DSMAX=0.1, UZR={26: [31.0]}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
 a.run(starting_point='UZ1', c='qif', ICP=6, NPAR=n_params, NDIM=n_dim, name='D_rs:4', origin='D_rs/I_fs:4:hb1',
       NMX=8000, DSMAX=0.1, UZR={6: [0.1]}, STOP=["UZ1"], NPR=100, RL0=0.0, DS="-")
-a.run(starting_point='UZ1', c='qif', ICP=30, NPAR=n_params, NDIM=n_dim, name='I_fs:4:2', origin='D_rs:4',
+a.run(starting_point='UZ1', c='qif', ICP=26, NPAR=n_params, NDIM=n_dim, name='I_fs:4:2', origin='D_rs:4',
       NMX=8000, DSMAX=0.1, UZR={}, STOP=[], NPR=100, RL1=100.0, RL0=0.0)
-a.run(starting_point='LP1', c='qif2', ICP=[6, 30], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:4:lp1', origin='I_fs:4:2',
+a.run(starting_point='LP1', c='qif2', ICP=[6, 26], NPAR=n_params, NDIM=n_dim, name='D_rs/I_fs:4:lp1', origin='I_fs:4:2',
       NMX=8000, DSMAX=0.1, UZR={}, STOP=['CP2'], NPR=10, RL1=10.0, RL0=0.0, bidirectional=True, EPSS=1e-6)
 
 # save results
--- Izhikevich/config/eic_shadowing.f90
+++ Izhikevich/config/eic_shadowing.f90
@@ -6,9 +6,8 @@ contains
 
 
 subroutine eic_run(t,y,dy,v_t,v_r,k,g_gabaa,g_ampa,Delta,C,E_gabaa,&
-     & E_ampa,I_ext,b,a,d,tau_ampa,tau_gabaa,v_t_0,v_r_0,k_0,g_gabaa_0,&
-     & g_ampa_0,Delta_0,C_0,E_gabaa_0,E_ampa_0,I_ext_0,b_0,a_0,d_0,&
-     & tau_ampa_0,tau_gabaa_0,weight,weight_0,weight_1,weight_2)
+     & E_ampa,I_ext,b,a,d,tau_ampa,tau_gabaa,v_t_0,v_r_0,k_0,&
+     & Delta_0,C_0,I_ext_0,b_0,a_0,d_0,weight,weight_0,weight_1,weight_2)
 
 implicit none
 
@@ -17,17 +16,15 @@ double precision, intent(in) :: y(10)
 double precision :: r
 double precision :: v
 double precision :: u
-double precision :: s_ampa
-double precision :: s_gabaa
+double precision :: s
 double precision :: r_0
 double precision :: v_0
 double precision :: u_0
-double precision :: s_ampa_0
-double precision :: s_gabaa_0
-double precision :: r_e
-double precision :: r_i
-double precision :: r_e_0
-double precision :: r_i_0
+double precision :: s_0
+double precision :: s_e
+double precision :: s_i
+double precision :: s_e_0
+double precision :: s_i_0
 double precision, intent(inout) :: dy(10)
 double precision, intent(in) :: v_t
 double precision, intent(in) :: v_r
@@ -47,18 +44,12 @@ double precision, intent(in) :: tau_gabaa
 double precision, intent(in) :: v_t_0
 double precision, intent(in) :: v_r_0
 double precision, intent(in) :: k_0
-double precision, intent(in) :: g_gabaa_0
-double precision, intent(in) :: g_ampa_0
 double precision, intent(in) :: Delta_0
 double precision, intent(in) :: C_0
-double precision, intent(in) :: E_gabaa_0
-double precision, intent(in) :: E_ampa_0
 double precision, intent(in) :: I_ext_0
 double precision, intent(in) :: b_0
 double precision, intent(in) :: a_0
 double precision, intent(in) :: d_0
-double precision, intent(in) :: tau_ampa_0
-double precision, intent(in) :: tau_gabaa_0
 double precision, intent(in) :: weight
 double precision, intent(in) :: weight_0
 double precision, intent(in) :: weight_1
@@ -68,38 +59,34 @@ double precision, intent(in) :: weight_2
 r = y(1)
 v = y(2)
 u = y(3)
-s_ampa = y(4)
-s_gabaa = y(5)
-r_0 = y(6)
-v_0 = y(7)
-u_0 = y(8)
-s_ampa_0 = y(9)
-s_gabaa_0 = y(10)
-r_e = r*weight
-r_i = r_0*weight_0
-r_e_0 = r*weight_1
-r_i_0 = r_0*weight_2
+s = y(4)
+r_0 = y(5)
+v_0 = y(6)
+u_0 = y(7)
+s_0 = y(8)
+s_e = s*weight
+s_i = s_0*weight_0
+s_e_0 = s*weight_1
+s_i_0 = s_0*weight_2
 
-dy(1) = (r*(-g_ampa*s_ampa - g_gabaa*s_gabaa &
+dy(1) = (r*(-g_ampa*s_e - g_gabaa*s_i &
      & + k*(2.0*v - v_r - v_t)) + Delta*k**2*(v - v_r)/(pi*C))/C
 dy(2) = (-pi*C*r*(pi*C*r/k &
-     & + Delta) + I_ext + g_ampa*s_ampa*(E_ampa &
+     & + Delta) + I_ext + g_ampa*s_e*(E_ampa &
      & - v) + g_gabaa&
-     & *s_gabaa*(E_gabaa - v) + k*v*(v - v_r - v_t) + k*v_r*v_t - u)/C
+     & *s_i*(E_gabaa - v) + k*v*(v - v_r - v_t) + k*v_r*v_t - u)/C
 dy(3) = a*(b*(v - v_r) - u) + d*r
-dy(4) = r_e - s_ampa/tau_ampa
-dy(5) = r_i - s_gabaa/tau_gabaa
-dy(6) = (r_0*(-g_ampa_0*s_ampa_0 - g_gabaa_0*s_gabaa_0 &
+dy(4) = r - s/tau_ampa
+dy(5) = (r_0*(-g_ampa*s_e_0 - g_gabaa*s_i_0 &
      & + k_0*(2.0*v_0 - v_r_0 - v_t_0)) + Delta_0*k_0**2*(v_0 - v_r_0)&
      & /(pi*C_0))/C_0
-dy(7) = (-pi*C_0*r_0*(pi*C_0*r_0/k_0 &
-     & + Delta_0) + I_ext_0 + g_ampa_0*s_ampa_0*(E_ampa_0 &
-     & - v_0) + g_gabaa_0&
-     & *s_gabaa_0*(E_gabaa_0 - v_0) &
+dy(6) = (-pi*C_0*r_0*(pi*C_0*r_0/k_0 &
+     & + Delta_0) + I_ext_0 + g_ampa*s_e_0*(E_ampa &
+     & - v_0) + g_gabaa&
+     & *s_i_0*(E_gabaa - v_0) &
      & + k_0*v_0*(v_0 - v_r_0 - v_t_0) + k_0*v_r_0*v_t_0 - u_0)/C_0
-dy(8) = a_0*(b_0*(v_0 - v_r_0) - u_0) + d_0*r_0
-dy(9) = r_e_0 - s_ampa_0/tau_ampa_0
-dy(10) = r_i_0 - s_gabaa_0/tau_gabaa_0
+dy(7) = a_0*(b_0*(v_0 - v_r_0) - u_0) + d_0*r_0
+dy(8) = r_0 - s_0/tau_gabaa
 
 end subroutine
 
@@ -120,8 +107,7 @@ call eic_run(args(14), y, dy, args(1), args(2), args(3), args(4), &
      & args(5), args(6), args(7), args(8), args(9), args(15), args(16),&
      &  args(17), args(18), args(19), args(20), args(21), args(22), &
      & args(23), args(24), args(25), args(26), args(27), args(28), &
-     & args(29), args(30), args(31), args(32), args(33), args(34), &
-     & args(35), args(36), args(37), args(38), args(39))
+     & args(29), args(30), args(31), args(32), args(33))
 
 end subroutine func
 
@@ -151,32 +137,24 @@ args(20) = 8.0  ! tau_gabaa
 args(21) = -40.0  ! v_t_0
 args(22) = -55.0  ! v_r_0
 args(23) = 1.0  ! k_0
-args(24) = 1.0  ! g_gabaa_0
-args(25) = 1.0  ! g_ampa_0
-args(26) = 0.5  ! Delta_0
-args(27) = 20.0  ! C_0
-args(28) = -65.0  ! E_gabaa_0
-args(29) = 0.0  ! E_ampa_0
-args(30) = 0.0  ! I_ext_0
-args(31) = 0.025  ! b_0
-args(32) = 0.2  ! a_0
-args(33) = 0.0  ! d_0
-args(34) = 6.0  ! tau_ampa_0
-args(35) = 8.0  ! tau_gabaa_0
-args(36) = 16.0  ! weight
-args(37) = 16.0  ! weight_0
-args(38) = 4.0  ! weight_1
-args(39) = 4.0  ! weight_2
+args(24) = 0.5  ! Delta_0
+args(25) = 20.0  ! C_0
+args(26) = 0.0  ! I_ext_0
+args(27) = 0.025  ! b_0
+args(28) = 0.2  ! a_0
+args(29) = 0.0  ! d_0
+args(30) = 15.0  ! k_ee
+args(31) = 10.0  ! k_ei
+args(32) = 5.0  ! k_ie
+args(33) = 10.0  ! k_ii
 y(1) = 0.02  ! r
 y(2) = -45.0  ! v
 y(3) = 0.0  ! u
-y(4) = 0.0  ! s_ampa
-y(5) = 0.0  ! s_gabaa
-y(6) = 0.0  ! r_0
-y(7) = -60.0  ! v_0
-y(8) = 0.0  ! u_0
-y(9) = 0.0  ! s_ampa_0
-y(10) = 0.0  ! s_gabaa_0
+y(4) = 0.0  ! s_e
+y(5) = 0.0  ! r_0
+y(6) = -60.0  ! v_0
+y(7) = 0.0  ! u_0
+y(8) = 0.0  ! s_i
 
 end subroutine stpnt
 
