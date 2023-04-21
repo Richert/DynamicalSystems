8fe15c4b04ef3eab958df433be6d3ff74a44dbac
Richert <richgast.90@gmail.com>
2023-04-21 17:39:35 -0500

    updates worker script for the arnold tongue simulations

--- Izhikevich/sweep_eic_bump.py
+++ Izhikevich/sweep_eic_bump.py
@@ -7,7 +7,7 @@ sweep["Delta_i"] = np.asarray([0.2, 2.0])
 sweep["trial"] = np.arange(0, 10)
 
 # save sweep to file
-fname = "bump_sweep"
+fname = "eic_sweep"
 with open(f"config/{fname}.pkl", "wb") as f:
     pickle.dump(sweep, f)
     f.close()
--- Izhikevich/worker_eic_bump.py
+++ Izhikevich/worker_eic_bump.py
@@ -1,8 +1,9 @@
+import torch.cuda
 from rectipy import Network, circular_connectivity
 from pyrates import NodeTemplate, CircuitTemplate
 import sys
-cond, wdir, tdir = sys.argv[-3:]
-sys.path.append(wdir)
+# cond, wdir, tdir = sys.argv[-3:]
+# sys.path.append(wdir)
 sys.path.append("~/PycharmProjects/DynamicalSystems/reservoir_computing")
 import numpy as np
 from scipy.stats import cauchy
@@ -38,16 +39,16 @@ def dist(x: int, method: str = "inverse", zero_val: float = 1.0, inverse_pow: fl
 device = "cuda:0"
 
 # working directory
-# wdir = "config"
-# tdir = "results"
+wdir = "config"
+tdir = "results"
 
 # sweep condition
-# cond = 94
+cond = 12
 p1 = "Delta_i"
 p2 = "trial"
 
 # parameter sweep definition
-with open(f"{wdir}/bump_sweep.pkl", "rb") as f:
+with open(f"{wdir}/eic_sweep.pkl", "rb") as f:
     sweep = pickle.load(f)
     v1s = sweep[p1]
     v2s = sweep[p2]
@@ -57,7 +58,7 @@ v1, v2 = vals[int(cond)]
 print(f"Condition: {p1} = {v1},  {p2} = {v2}")
 
 # general parameters
-N = 2000
+N = 1000
 p = 0.2
 v_spike = 1e3
 v_reset = -1e3
@@ -101,7 +102,7 @@ cutoff = 500.0
 T = 3000.0
 dt = 1e-2
 sr = 10
-p_in_vals = 10.0**np.linspace(-2.0, 0.0, num=20)
+p_in_vals = 10.0**np.linspace(-1.0, -0.1, num=2)
 
 # time-averaging parameters
 sigma = 200
@@ -126,43 +127,6 @@ W_ie = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indi
 W_ei = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, i_pdfs)), homogeneous_weights=False)
 W_ii = circular_connectivity(N, p, spatial_distribution=rv_discrete(values=(indices, i_pdfs)), homogeneous_weights=False)
 
-# initialize nodes
-e_vars = {"C": Ce, "k": ke, "v_r": ve_r, "v_theta": spike_thresholds_e, "eta": Ie, "tau_u": 1 / ae, "b": be,
-          "kappa": de,
-          "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_ampa, "v": ve_t}
-i_vars = {"C": Ci, "k": ki, "v_r": vi_r, "v_theta": spike_thresholds_i, "eta": Ii, "tau_u": 1 / ai, "b": bi,
-          "kappa": di,
-          "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_gaba, "v": vi_t}
-rs = NodeTemplate.from_yaml("config/ik_snn/ik")
-fs = NodeTemplate.from_yaml("config/ik_snn/ik")
-
-# construct rs and fs circuits
-rs_neurons = {f'rs_{i}': rs for i in range(N)}
-rs_net = CircuitTemplate("rs", nodes=rs_neurons)
-rs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ee * k_ee,
-                             source_nodes=list(rs_neurons.keys()))
-rs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in e_vars.items()})
-fs_neurons = {f'fs_{i}': fs for i in range(N)}
-fs_net = CircuitTemplate("fs", nodes=fs_neurons)
-fs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ii * k_ii,
-                             source_nodes=list(fs_neurons.keys()))
-fs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in i_vars.items()})
-
-# combine the rs and fs populations into a single circuit
-net = CircuitTemplate("eic", circuits={"rs": rs_net, "fs": fs_net})
-net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ie * k_ie,
-                          source_nodes=[f"rs/rs_{i}" for i in range(N)],
-                          target_nodes=[f"fs/fs_{i}" for i in range(N)])
-net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ei * k_ei,
-                          source_nodes=[f"fs/fs_{i}" for i in range(N)],
-                          target_nodes=[f"rs/rs_{i}" for i in range(N)])
-
-# initialize rectipy model
-model = Network(dt=dt, device="cuda:0")
-model.add_diffeq_node("eic", node=net, input_var="ik_op/I_ext", output_var="rs/all/ik_op/s",
-                      spike_var="ik_op/spike", spike_def="all/all/ik_op/v", spike_reset=v_reset,
-                      spike_threshold=v_spike, verbose=False, clear=True)
-
 # simulation
 ############
 
@@ -172,16 +136,53 @@ results = {"sweep": {p1: v1, p2: v2}, "T": T, "dt": dt, "sr": sr, "p": p, "popul
 
 for i, p_in in enumerate(p_in_vals):
 
+    # initialize nodes
+    e_vars = {"C": Ce, "k": ke, "v_r": ve_r, "v_theta": spike_thresholds_e, "eta": Ie, "tau_u": 1 / ae, "b": be,
+              "kappa": de,
+              "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_ampa, "v": ve_t}
+    i_vars = {"C": Ci, "k": ki, "v_r": vi_r, "v_theta": spike_thresholds_i, "eta": Ii, "tau_u": 1 / ai, "b": bi,
+              "kappa": di,
+              "g_e": g_ampa, "E_e": E_ampa, "g_i": g_gaba, "E_i": E_gaba, "tau_s": tau_gaba, "v": vi_t}
+    rs = NodeTemplate.from_yaml("config/ik_snn/ik")
+    fs = NodeTemplate.from_yaml("config/ik_snn/ik")
+
+    # construct rs and fs circuits
+    rs_neurons = {f'rs_{i}': rs for i in range(N)}
+    rs_net = CircuitTemplate("rs", nodes=rs_neurons)
+    rs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ee * k_ee,
+                                 source_nodes=list(rs_neurons.keys()))
+    rs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in e_vars.items()})
+    fs_neurons = {f'fs_{i}': fs for i in range(N)}
+    fs_net = CircuitTemplate("fs", nodes=fs_neurons)
+    fs_net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ii * k_ii,
+                                 source_nodes=list(fs_neurons.keys()))
+    fs_net.update_var(node_vars={f"all/ik_op/{key}": val for key, val in i_vars.items()})
+
+    # combine the rs and fs populations into a single circuit
+    net = CircuitTemplate("eic", circuits={"rs": rs_net, "fs": fs_net})
+    net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_e", weight=W_ie * k_ie,
+                              source_nodes=[f"rs/rs_{i}" for i in range(N)],
+                              target_nodes=[f"fs/fs_{i}" for i in range(N)])
+    net.add_edges_from_matrix(source_var="ik_op/s", target_var="ik_op/s_i", weight=W_ei * k_ei,
+                              source_nodes=[f"fs/fs_{i}" for i in range(N)],
+                              target_nodes=[f"rs/rs_{i}" for i in range(N)])
+
+    # initialize rectipy model
+    model = Network(dt=dt, device="cuda:0")
+    model.add_diffeq_node("eic", node=net, input_var="ik_op/I_ext", output_var="rs/all/ik_op/s",
+                          spike_var="ik_op/spike", spike_def="all/all/ik_op/v", spike_reset=v_reset,
+                          spike_threshold=v_spike, verbose=False, clear=True, to_file=False)
+
     # define inputs
     n_inputs = int(N * p_in)
     center = int(N*0.5)
     inp_indices = np.arange(center-int(0.5*n_inputs), center+int(0.5*n_inputs))
-    inp = np.zeros((int(T/dt), N))
-    inp[:int(cutoff*0.5/dt), :N] -= 30.0
-    inp[int(1000/dt):int(1500/dt), inp_indices] += 30.0
+    inp = np.zeros((int(T/dt), 2*N))
+    inp[:int(cutoff*0.5/dt), :N] -= 20.0
+    inp[int(1000/dt):int(1500/dt), inp_indices] += 20.0
 
     # perform simulation
-    obs = model.run(inputs=inp, sampling_steps=sr, record_output=True, verbose=False)
+    obs = model.run(inputs=inp, sampling_steps=sr, record_output=True, verbose=False, enable_grad=False)
     res = obs.to_numpy("out")
 
     # calculate the distribution of the time-averaged network activity after the stimulation was turned off
@@ -196,23 +197,20 @@ for i, p_in in enumerate(p_in_vals):
     results["population_dists"].append(population_dist)
     results["p_in"].append(p_in)
 
-    # reset model
-    model.reset()
-
     # plot results
-    # fig, axes = plt.subplots(nrows=2, figsize=(12, 8))
-    # ax = axes[0]
-    # im = ax.imshow(s.T, aspect=4.0, interpolation="none")
-    # plt.colorbar(im, ax=ax, shrink=0.8)
-    # ax.set_xlabel('time')
-    # ax.set_ylabel('neurons')
-    # ax = axes[1]
-    # ax.plot(target_dist, label="target")
-    # ax.plot(population_dist, label="SNN")
-    # ax.set_xlabel("neurons")
-    # ax.set_ylabel("probability")
-    # plt.tight_layout()
-    # plt.show()
+    fig, axes = plt.subplots(nrows=2, figsize=(12, 8))
+    ax = axes[0]
+    im = ax.imshow(s.T, aspect=4.0, interpolation="none")
+    plt.colorbar(im, ax=ax, shrink=0.8)
+    ax.set_xlabel('time')
+    ax.set_ylabel('neurons')
+    ax = axes[1]
+    ax.plot(target_dist, label="target")
+    ax.plot(population_dist, label="SNN")
+    ax.set_xlabel("neurons")
+    ax.set_ylabel("probability")
+    plt.tight_layout()
+    plt.show()
 
 # save results
 fname = f"snn_bump"
