7ee5c2c07a0d463c7a915a2ac8c0b033449fcbb8
Richert <richgast.90@gmail.com>
2023-05-11 23:29:47 -0500

    updated worker script for the two-population bump simulations

--- Izhikevich/plotting_snn_entrainment_single.py
+++ Izhikevich/plotting_snn_entrainment_single.py
@@ -6,12 +6,18 @@ import matplotlib.pyplot as plt
 import h5py
 
 
+def mse(x: np.ndarray, y: np.ndarray) -> float:
+    x = x.squeeze() / np.max(x.flatten())
+    y = y.squeeze() / np.max(y.flatten())
+    return float(np.mean((x - y)**2))
+
+
 # load data set
 fname = "results/entrainment/snn_entrainment_0.h5"
 data = h5py.File(fname, "r")
 
-res_dict = {"alpha": [], "dim": [], "seq": [], "train_loss": [], "test_loss": [], "kernel_width": [],
-            "kernel_variance": []}
+res_dict = {"alpha": [], "dim": [], "seq": [], "train_loss_1": [], "train_loss_2": [], "test_loss_1": [],
+            "test_loss_2": [], "kernel_width": [], "kernel_variance": []}
 
 for i in range(len(data)-1):
 
@@ -19,11 +25,29 @@ for i in range(len(data)-1):
     res_dict["alpha"].append(np.asarray(g["alpha"]))
     res_dict["dim"].append(np.asarray(g["dimensionality"]))
     res_dict["seq"].append(np.asarray(g["sequentiality"]))
+    res_dict["train_loss_1"].append(mse(np.asarray(g["targets"][0]), np.asarray(g["train_predictions"][0])))
+    res_dict["train_loss_2"].append(mse(np.asarray(g["targets"][1]), np.asarray(g["train_predictions"][1])))
+    test_losses = [mse(np.asarray(g["targets"][0]), np.asarray(sig)) for sig in g["test_predictions"][0]]
+    res_dict["test_loss_1"].append(np.mean(test_losses))
+    test_losses = [mse(np.asarray(g["targets"][1]), np.asarray(sig)) for sig in g["test_predictions"][1]]
+    res_dict["test_loss_2"].append(np.mean(test_losses))
 
-fig, axes = plt.subplots(ncols=2, figsize=(12, 5))
+fig, axes = plt.subplots(ncols=4, figsize=(12, 4))
 ax = axes[0]
 ax.plot(res_dict["alpha"], res_dict["dim"])
+ax.set_title("dim")
+ax.set_xlabel("alpha")
 ax = axes[1]
 ax.plot(res_dict["alpha"], res_dict["seq"])
+ax.set_title("seq")
+ax.set_xlabel("alpha")
+ax = axes[2]
+ax.plot(res_dict["alpha"], res_dict["train_loss_1"])
+ax.set_title("train loss")
+ax.set_xlabel("alpha")
+ax = axes[3]
+ax.plot(res_dict["alpha"], res_dict["test_loss_1"])
+ax.set_title("test loss")
+ax.set_xlabel("alpha")
 plt.tight_layout()
 plt.show()
