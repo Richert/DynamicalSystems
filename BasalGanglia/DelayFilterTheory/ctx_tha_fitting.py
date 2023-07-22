05c680000418df372a5e8eebb0f8669424d89bd3
Richert <richgast.90@gmail.com>
2023-07-22 07:57:20 -0500

    added scripts for the basal ganglia delay filter project

--- BasalGanglia/DelayFilterTheory/ctx_tha_fitting.py
+++ BasalGanglia/DelayFilterTheory/ctx_tha_fitting.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 from rectipy import FeedbackNetwork, random_connectivity
 
 
@@ -13,18 +14,19 @@ def training_data(stim_amp: float, stim_dur: float, f1: float, f2: float, n: int
 
     # create single input-target pair
     target = np.sin(2.0*np.pi*f1*t) * np.sin(2.0*np.pi*f2*t)
-    inp = np.zeros((steps, 1))
-    inp[:int(stim_dur/dt), 0] = stim_amp
+    inp = np.zeros((steps,))
+    inp[:int(stim_dur/dt)] = stim_amp
 
     # create multi-trial input-target vectors
-    starts = np.random.randint(low=min_isi, high=max_isi, size=(n,))
-    inputs = np.zeros((int(n*steps + np.sum(starts)), 1))
-    targets = np.zeros((int(n*steps + np.sum(starts)),))
-    for i in range(n):
-        start = i * steps + starts[i]
-        stop = (i+1) * steps + starts[i]
-        inputs[start:stop] = inp
-        targets[start:stop] = target
+    inputs = []
+    targets = []
+    for start in np.random.randint(low=min_isi, high=max_isi, size=(n,)):
+        inp_tmp = np.zeros((steps + start, 1))
+        inp_tmp[start:, 0] = inp
+        target_tmp = np.zeros_like(inp_tmp)
+        target_tmp[start:, 0] = target
+        inputs.append(inp_tmp)
+        targets.append(target_tmp)
 
     return inputs, targets
 
@@ -33,6 +35,7 @@ def training_data(stim_amp: float, stim_dur: float, f1: float, f2: float, n: int
 n_ctx = 200
 n_tha = 20
 n_in = 1
+n_out = 1
 p = 0.2
 k = 2.0
 dt = 1e-2
@@ -56,38 +59,40 @@ net.add_diffeq_node("ctx", node, input_var="s_ext", output_var="s", weights=W_cc
 net.add_diffeq_node("tha", node, input_var="s_ext", output_var="s", weights=W_tt, source_var="s",
                     target_var="s_in", op="rs_op", spike_var="spike", spike_def="v")
 net.add_func_node("inp", n_in, activation_function="identity")
+net.add_func_node("out", n_out, activation_function="identity")
 
 # add network edges
 net.add_edge("inp", "tha", weights=np.random.rand(n_in, n_tha))
 net.add_edge("tha", "ctx", weights=k*W_ct, train="gd")
 net.add_edge("ctx", "tha", weights=k*W_tc, train="gd", feedback=True)
+net.add_edge("ctx", "out", weights=np.random.rand(n_ctx, n_out))
 
 # perform simulation of dynamics
 ################################
 
-# define input
-steps = 10000
-inp = np.zeros((steps, n_in)) + 0.0
-inp[3000:6000] += 0.2
-
-# perform simulation
-obs = net.run(inp, sampling_steps=10, enable_grad=False, record_vars=[("ctx", "v", False), ("tha", "v", False)])
-
-# plot results
-fig, axes = plt.subplots(nrows=3, figsize=(12, 9))
-ax = axes[0]
-ax.plot(obs.to_numpy(("ctx", "v")))
-ax.set_title("ctx")
-ax.set_ylabel("v")
-ax = axes[1]
-ax.plot(obs.to_numpy(("tha", "v")))
-ax.set_title("tha")
-ax.set_ylabel("v")
-ax = axes[2]
-ax.plot(np.mean(obs.to_numpy("out"), axis=1))
-ax.set_title("ctx")
-ax.set_ylabel("s")
-plt.show()
+# # define input
+# steps = 10000
+# inp = np.zeros((steps, n_in)) + 0.0
+# inp[3000:6000] += 0.2
+#
+# # perform simulation
+# obs = net.run(inp, sampling_steps=10, enable_grad=False, record_vars=[("ctx", "v", False), ("tha", "v", False)])
+#
+# # plot results
+# fig, axes = plt.subplots(nrows=3, figsize=(12, 9))
+# ax = axes[0]
+# ax.plot(obs.to_numpy(("ctx", "v")))
+# ax.set_title("ctx")
+# ax.set_ylabel("v")
+# ax = axes[1]
+# ax.plot(obs.to_numpy(("tha", "v")))
+# ax.set_title("tha")
+# ax.set_ylabel("v")
+# ax = axes[2]
+# ax.plot(obs.to_numpy("out"))
+# ax.set_title("readout")
+# ax.set_ylabel("")
+# plt.show()
 
 # train network to generate a target function
 #############################################
@@ -95,5 +100,28 @@ plt.show()
 # get inputs and targets
 inputs, targets = training_data(40.0, 10.0, 0.006, 0.012, 40, 10000, 50000, dt)
 
+# choose sampling rate
+sr = 10
+targets = [t[::sr, :] for t in targets]
+
 # perform weight optimization
-obs = net.fit_bptt(inputs, targets)
+obs = net.fit_bptt(inputs, targets, optimizer="rprop", optimizer_kwargs={"etas": (0.5, 1.2)},
+                   sampling_steps=sr, update_steps=100000)
+
+# get test data
+in_test, tar_test = training_data(40.0, 10.0, 0.006, 0.012, 3, 10000, 50000, dt)
+in_test = torch.vstack(in_test)
+tar_test = torch.vstack(tar_test).numpy()
+obs_test = net.run(in_test, sampling_steps=sr, enable_grad=False)
+
+# plot results
+fig, axes = plt.subplots(nrows=3, figsize=(12, 6))
+ax = axes[0]
+ax.plot(obs.to_numpy("epochs"), obs.to_numpy("epoch_loss"))
+ax.set_title("MSE")
+ax = axes[1]
+ax.plot(obs_test.to_numpy("out"), label="prediction")
+ax.plot(tar_test[::sr, 0], label="target")
+ax.set_title("Fit")
+ax.legend()
+plt.show()
