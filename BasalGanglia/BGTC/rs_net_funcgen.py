619cc74cf410e87141d0da5140d550a657d0dfbf
Richert <richgast.90@gmail.com>
2023-10-31 18:17:35 -0500

    updated analysis script for mean-field impulse response decay time constants

--- BasalGanglia/BGTC/rs_net_funcgen.py
+++ BasalGanglia/BGTC/rs_net_funcgen.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import torch
-from rectipy import Network, random_connectivity
+from rectipy import Network, random_connectivity, FeedbackNetwork
 import numpy as np
 from scipy.stats import cauchy
 from torch.nn import MSELoss
@@ -25,7 +25,7 @@ def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
 device = "cpu"
 
 # network parameters
-N = 800
+N = 1000
 p = 0.2
 v_spike = 1e3
 v_reset = -1e3
@@ -35,8 +35,8 @@ C_e = 100.0   # unit: pF
 k_e = 0.7  # unit: None
 v_r_e = -60.0  # unit: mV
 v_t_e = -40.0  # unit: mV
-Delta_e = 0.5  # unit: mV
-eta_e = 50.0  # unit: pA
+Delta_e = 0.59  # unit: mV
+eta_e = 49.25  # unit: pA
 d_e = 100.0  # unit: pA
 a_e = 0.03  # unit: 1/ms
 b_e = -2.0  # unit: nS
@@ -48,14 +48,14 @@ E_i = -65.0  # unit: mV
 spike_thresholds_e = lorentzian(N, eta=v_t_e, delta=Delta_e, lb=v_r_e, ub=2*v_t_e - v_r_e)
 
 # input parameters
-freqs = [6.0, 10.0]
+freqs = [6.0, 12.0]
 n_in = len(freqs)
 n_out = 1
 
 # define connectivity
 k_ee = 15.0
-k_in = 5.0
-W_in = np.random.rand(N, n_in) * k_in
+k_in = 10.0
+W_in = np.random.randn(N, n_in) * k_in
 W_out = np.random.randn(n_out, N)
 W_ee = random_connectivity(N, N, p, normalize=True) * k_ee
 
@@ -65,7 +65,7 @@ T_epoch = 500.0 * t_scale
 T_init = [300.0*t_scale, 500.0*t_scale]
 dt = 1e-2
 dts = 1.0
-n_epochs = 10
+n_epochs = 100
 
 # setup model
 #############
@@ -75,18 +75,19 @@ pc_vars = {"C": C_e, "k": k_e, "v_r": v_r_e, "v_t": spike_thresholds_e, "eta": e
            "b": b_e, "d": d_e, "tau_s": tau_s, "v": v_t_e, "E_e": E_e, "E_i": E_i}
 
 # initialize network
-net = Network(dt=dt, device=device)
+net = FeedbackNetwork(dt=dt, device=device)
 
 # add network nodes
 net.add_diffeq_node("rnn", "config/ik_snn/ik", input_var="I_ext", output_var="s",
                     weights=W_ee, source_var="s", target_var="s_e", spike_var="spike", spike_def="v", op="ik_op", N=N,
                     node_vars=pc_vars)
-net.add_func_node("inp", n_in, activation_function="identity")
+net.add_func_node("inp", n_in, activation_function="sigmoid")
 net.add_func_node("out", n_out, activation_function="identity")
 
 # add network edges
 net.add_edge("inp", "rnn", weights=W_in, train="gd")
 net.add_edge("rnn", "out", weights=W_out, train="gd")
+net.add_edge("rnn", "inp", feedback=True, train="gd")
 
 # compile network
 net.compile()
@@ -98,8 +99,8 @@ net.compile()
 loss_fn = MSELoss()
 
 # setup optimizer
-optim = Rprop(net.parameters(), lr=0.1, etas=(0.5, 1.2), step_sizes=(1e-3, 100.0))
-# optim = Adadelta(net.parameters(), lr=20.0, rho=0.95, eps=1e-6)
+# optim = Rprop(net.parameters(), lr=0.1, etas=(0.5, 1.2), step_sizes=(1e-3, 100.0))
+optim = Adadelta(net.parameters(), lr=50.0, rho=0.99, eps=1e-6)
 
 # run optimization
 y0 = net.state
@@ -109,6 +110,7 @@ for epoch in range(n_epochs):
     # get initial state
     init_steps = int(np.random.uniform(low=T_init[0], high=T_init[1])/dt)
     net.run(inputs=np.zeros((init_steps, n_in)), sampling_steps=init_steps, enable_grad=False, verbose=False)
+    y1 = net.state
 
     # define input
     inp = np.zeros((int(T_epoch/dt), n_in))
@@ -131,14 +133,16 @@ for epoch in range(n_epochs):
     optim.zero_grad()
     loss.backward()
     optim.step()
-    net.detach(requires_grad=True, detach_params=False)
+    # net.detach(requires_grad=True, detach_params=False)
+    net.reset(y0)
 
     # save stuff for plotting
     losses.append(loss.item())
-    print(f"Epoch #{epoch} finished. Current loss = {losses[0]}")
+    print(f"Epoch #{epoch} finished. Current loss = {losses[-1]}")
 
 # get fitted weights
-w1 = list(net.parameters())[-1].detach().cpu().numpy()
+ws = [p.detach().cpu().numpy() for p in net.parameters()]
+w1 = ws[0] if ws[0].shape[0] == 1 else ws[1]
 
 # get fitted model predictions
 results = {"input": [], "target": [], "prediction": [], "rnn": []}
@@ -154,6 +158,7 @@ for idx in range(n_in):
     target[:, 0] = np.sin(freqs[idx]*np.pi*2.0*t)
 
     # run model
+    net.reset(y1)
     obs = net.run(inputs=inp, enable_grad=False, sampling_steps=1, verbose=False, record_vars=[("rnn", "out", False)])
 
     # store results
