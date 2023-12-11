283542f7b3a6b3c534c880171335f3ae027be542
Richert <richgast.90@gmail.com>
2023-12-11 17:08:06 -0600

    new scripts for an Izhikevich neuron with adaptive spike threshold

--- Izhikevich/simulation_sfa_snn.py
+++ Izhikevich/simulation_sfa_snn.py
@@ -28,14 +28,15 @@ k = 0.7  # unit: None
 v_r = -60.0  # unit: mV
 v_t = -40.0  # unit: mV
 eta = 0.0  # unit: pA
-Delta = 1.0  # unit: pA
-kappa = 50.0
-tau_u = 200.0
-b = -2.0
+Delta = 5.0
+kappa = 5.0
+tau_u = 35.0
+b = -5.0
 tau_s = 6.0
 tau_x = 300.0
-g = 15.0
+g = 5.0
 E_r = 0.0
+
 v_reset = -1000.0
 v_peak = 1000.0
 
@@ -44,29 +45,30 @@ T = 3500.0
 dt = 1e-2
 dts = 1e-1
 cutoff = int(500.0/dt)
-inp = np.zeros((int(T/dt), 1)) + 35.0
-inp[:int(cutoff/dt)] -= 30.0
-inp[int(1000/dt):int(2000/dt),] += 100.0
+inp = np.zeros((int(T/dt), 1)) + 300.0
+# inp[:int(cutoff/dt), 0] -= 30.0
+inp[int(1000/dt):int(2000/dt), 0] -= 100.0
 
 # define lorentzian of etas
-bs = lorentzian(N, eta=b, delta=Delta, lb=b-10*Delta, ub=b+10*Delta)
+# bs = lorentzian(N, eta=b, delta=Delta, lb=b-10*Delta, ub=b+10*Delta)
+etas = eta + Delta * np.tan(0.5*np.pi*(2*np.arange(1, N+1)-N-1)/(N+1))
 
 # define connectivity
-W = random_connectivity(N, N, 0.2)
+# W = random_connectivity(N, N, 0.2)
 
 # run the model
 ###############
 
 # initialize model
-node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": v_t, "eta": eta, "tau_u": tau_u, "b": bs, "kappa": kappa,
+node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": v_t, "eta": etas, "tau_u": tau_u, "b": b, "kappa": kappa,
              "g": g, "E_r": E_r, "tau_s": tau_s, "v": v_r, "tau_x": tau_x}
 
 # initialize model
 net = Network(dt=dt, device="cpu")
-net.add_diffeq_node("sfa", f"config/ik_snn/sfa", weights=W, source_var="s", target_var="s_in",
+net.add_diffeq_node("sfa", f"config/ik_snn/sfa", #weights=W, source_var="s", target_var="s_in",
                     input_var="I_ext", output_var="s", spike_var="spike", reset_var="v", to_file=False,
                     node_vars=node_vars.copy(), op="sfa_op", spike_reset=v_reset, spike_threshold=v_peak,
-                    verbose=False, clear=True)
+                    verbose=False, clear=True, N=N)
 
 # perform simulation
 obs = net.run(inputs=inp, sampling_steps=int(dts/dt), verbose=True, cutoff=cutoff)
