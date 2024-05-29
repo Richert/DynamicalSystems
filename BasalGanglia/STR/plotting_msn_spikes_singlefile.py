daef89909c422fede3721b55f0485e6f45aceadb
Richert <richgast.90@gmail.com>
2024-05-29 16:53:16 -0500

    new scripts for fitting a combination of Lorentzians to the recovery variables in SNNs

--- BasalGanglia/STR/plotting_msn_spikes_singlefile.py
+++ BasalGanglia/STR/plotting_msn_spikes_singlefile.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import loadmat
+from scipy.spatial.distance import pdist, squareform
 import os
 
 # choose condition to plot
 drug = "SKF38393"
-dose = "LowDose"
+dose = "Vehicle"
 mouse_id = "971"
-fields = ["events_5hz", "dff_traces_5hz", "speed_traces_5hz"]
+fields = ["events_5hz", "dff_traces_5hz", "speed_traces_5hz", "cellXYcoords_um"]
 
 # load data
 data = {}
@@ -18,55 +19,65 @@ for file in os.listdir(f"{path}/{drug}/{dose}"):
         data_tmp = loadmat(f"{path}/{drug}/{dose}/{file}/{condition}_drug.mat", simplify_cells=True)
         data[condition] = {field: data_tmp[f"{condition}_drug"][field] for field in fields}
 
-# calculate neuron covariances
+# calculate neuron covariances and spatial distances
 for key in data:
 
-    traces = data[key][fields[1]]
+    # covariance
+    traces = data[key][fields[0]]
     data[key]["cov"] = traces @ traces.T
 
+    # distance
+    coords = data[key][fields[-1]]
+    data[key]["dist"] = squareform(pdist(coords, metric="euclidean"))
+
 # plotting
-n_xticks = 5
+n_ticks = 5
 sr = 5
-max_neurons = 100
+max_neurons = 200
 for key in data:
 
     spikes = data[key][fields[0]]
-    speed = data[key][fields[-1]]
+    speed = data[key][fields[2]]
     n_steps = spikes.shape[1]
-    xticks = np.linspace(0, n_steps, n_xticks)
+    xticks = np.linspace(0, n_steps, n_ticks)
 
+    # plot network dynamics vs. movement speed
     fig, axes = plt.subplots(nrows=3, figsize=(12, 6), sharex=True)
-
     ax = axes[0]
     ax.imshow(spikes[:max_neurons, :], aspect="auto", cmap="Greys", vmin=0.0, vmax=0.2, interpolation="gaussian")
     ax.set_xticks(xticks, labels=[int(tick/sr) for tick in xticks])
     ax.set_xlabel("time (s)")
     ax.set_ylabel("neurons")
     ax.set_title(f"Spikes for condition: {key}")
-
     ax = axes[1]
     ax.plot(np.mean(spikes, axis=0)*sr)
     ax.set_xticks(xticks, labels=[int(tick / sr) for tick in xticks])
     ax.set_xlabel("time (s)")
     ax.set_ylabel("spike rate (Hz)")
     ax.set_title(f"Population rate for condition: {key}")
-
     ax = axes[2]
     ax.plot(speed)
     ax.set_xticks(xticks, labels=[int(tick / sr) for tick in xticks])
     ax.set_xlabel("time (s)")
     ax.set_ylabel("speed")
     ax.set_title(f"Mouse running velocity for condition: {key}")
-
     fig.suptitle(f"Mouse: {mouse_id}, drug: {drug}, dose: {dose}")
     plt.tight_layout()
 
-    fig, ax = plt.subplots(figsize=(7, 6))
-    im = ax.imshow(np.log(data[key]["cov"][:max_neurons, :max_neurons]), aspect="equal", cmap="viridis", interpolation="none")
+    # plot neuron covariance vs spatial distance
+    fig, axes = plt.subplots(ncols=2, figsize=(12, 6))
+    ax = axes[0]
+    im = ax.imshow(data[key]["cov"][:max_neurons, :max_neurons], aspect="equal", cmap="viridis", interpolation="none")
     plt.colorbar(im, ax=ax)
     ax.set_xlabel("neuron")
     ax.set_ylabel("neuron")
-    ax.set_title(f"Neuron covariances for condition: {key}")
+    ax.set_title(f"Covariance for condition: {key}")
+    ax = axes[1]
+    im = ax.imshow(data[key]["dist"][:max_neurons, :max_neurons], aspect="equal", cmap="viridis", interpolation="none")
+    plt.colorbar(im, ax=ax)
+    ax.set_xlabel("neuron")
+    ax.set_ylabel("neuron")
+    ax.set_title(f"Euclidean distance for condition: {key}")
     fig.suptitle(f"Mouse: {mouse_id}, drug: {drug}, dose: {dose}")
     plt.tight_layout()
 
