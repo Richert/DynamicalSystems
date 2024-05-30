8bf39c44e9080ecc9654b92254bf0124b5ba8784
Richert <richgast.90@gmail.com>
2024-05-29 21:40:14 -0500

    new scripts for fitting a combination of Lorentzians to the recovery variables in SNNs

---
+++ BasalGanglia/STR/plotting_msn_rate_dist.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import os
+from scipy.ndimage import gaussian_filter1d
+from pandas import DataFrame
+
+# choose condition
+drug = "SKF38393"
+dose = "Vehicle"
+spike_field = "dff_traces_5hz"
+speed_field = "speed_traces_5hz"
+
+# meta parameters
+speed_threshold = 0.6
+sigma_speed = 50
+sigma_rate = 3
+
+# analysis
+##########
+
+data = {"condition": [], "mouse": [], "rate": [], "speed": []}
+path = "/run/user/1000/gvfs/smb-share:server=fsmresfiles.fsm.northwestern.edu,share=fsmresfiles/Basic_Sciences/Phys/Kennedylab/Parkerlab/Calcium_v2"
+for file in os.listdir(f"{path}/{drug}/{dose}"):
+
+    # load data
+    _, mouse_id, *cond = file.split("_")
+    condition = "amph" if "amph" in cond else "veh"
+    data_tmp = loadmat(f"{path}/{drug}/{dose}/{file}/{condition}_drug.mat", simplify_cells=True)
+    spikes = data_tmp[f"{condition}_drug"][spike_field]
+    speed = data_tmp[f"{condition}_drug"][speed_field]
+
+    # calculate smooth variables
+    smoothed_spikes = np.asarray([gaussian_filter1d(spikes[i, :], sigma=sigma_rate) for i in range(spikes.shape[0])])
+    avg_sr = np.mean(smoothed_spikes, axis=0)
+    std_sr = np.std(smoothed_spikes, axis=0)
+    smoothed_speed = gaussian_filter1d(speed, sigma=sigma_speed)
+
+    # plotting
+    if condition == "veh":
+        fig, axes = plt.subplots(nrows=2, figsize=(12, 6), sharex=True)
+        ax = axes[0]
+        ax.plot(smoothed_speed, color="black")
+        ax.set_ylabel("speed")
+        ax = axes[1]
+        ax.plot(avg_sr * (smoothed_speed < speed_threshold), label="slow")
+        ax.plot(avg_sr * (smoothed_speed >= speed_threshold), label="fast")
+        ax.fill_between(x=np.arange(0, avg_sr.shape[0]), y1=avg_sr - 0.25*std_sr, y2=avg_sr + 0.25*std_sr, alpha=0.5,
+                        color="black")
+        ax.legend()
+        ax.set_ylabel("firing rate")
+        ax.set_xlabel("time")
+        plt.tight_layout()
+        plt.show()
+
+df = DataFrame.from_dict(data)
---
+++ BasalGanglia/STR/plotting_msn_rate_vs_speed.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import loadmat
+import os
+import seaborn as sns
+from pandas import DataFrame
+from scipy.ndimage import gaussian_filter1d
+
+# choose condition
+drug = "SKF38393"
+dose = "Vehicle"
+spike_field = "dff_traces_5hz"
+speed_field = "speed_traces_5hz"
+
+# meta parameters
+sigma_speed = 20
+sigma_rate = 20
+
+# analysis
+##########
+
+data = {"condition": [], "mouse": [], "rate": [], "speed": []}
+path = "/run/user/1000/gvfs/smb-share:server=fsmresfiles.fsm.northwestern.edu,share=fsmresfiles/Basic_Sciences/Phys/Kennedylab/Parkerlab/Calcium_v2"
+for file in os.listdir(f"{path}/{drug}/{dose}"):
+
+    # load data
+    _, mouse_id, *cond = file.split("_")
+    condition = "amph" if "amph" in cond else "veh"
+    data_tmp = loadmat(f"{path}/{drug}/{dose}/{file}/{condition}_drug.mat", simplify_cells=True)
+    rate = np.mean(data_tmp[f"{condition}_drug"][spike_field], axis=0)
+    speed = data_tmp[f"{condition}_drug"][speed_field]
+
+    # smooth data
+    rate = gaussian_filter1d(rate, sigma=sigma_rate)
+    speed = gaussian_filter1d(speed, sigma=sigma_speed)
+
+    # save data
+    n = rate.shape[0]
+    data["condition"].extend([condition] * n)
+    data["mouse"].extend([mouse_id] * n)
+    data["rate"].extend(rate.squeeze().tolist())
+    data["speed"].extend(speed.squeeze().tolist())
+    print(f"File {file} finished.")
+
+df = DataFrame.from_dict(data)
+
+# plotting
+##########
+
+g = sns.lmplot(data=df.loc[df["condition"] == "veh", :], x="speed", y="rate", hue="mouse", height=6, aspect=2)
+g.ax.set_title(f"condition: vehicle")
+plt.tight_layout()
+
+g = sns.lmplot(data=df.loc[df["condition"] == "amph", :], x="speed", y="rate", hue="mouse", height=6, aspect=2)
+g.ax.set_title(f"condition: amphetamine")
+plt.tight_layout()
+
+plt.show()
