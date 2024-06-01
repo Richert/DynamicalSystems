44a48520abd17baf981bea80f48650192eb6f272
Richert <richgast.90@gmail.com>
2024-06-01 15:44:15 -0500

    new scripts for fitting a combination of Lorentzians to the recovery variables in SNNs

--- BasalGanglia/STR/plotting_rates_vs_behavior.py
+++ BasalGanglia/STR/plotting_rates_vs_behavior.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from scipy.ndimage import gaussian_filter1d
 from pandas import read_csv, DataFrame
 
@@ -13,7 +14,7 @@ mouse = "m106"
 
 # meta parameters
 sigma_movement = 20
-sigma_rates = 5
+sigma_rates = 20
 sr = 5
 threshold = 0.02
 threshold2 = 0.08
@@ -40,7 +41,7 @@ markersize = 6
 ##########
 
 results = {"condition": [], "velocity": [], "acceleration": [], "angle": [], "angle_change": [],
-           "rate_mean": [], "state": [], "time": []}
+           "rate_mean": [], "rate_change": [], "state": [], "time": []}
 for cond in ["amph", "veh"]:
 
     # extract data
@@ -56,11 +57,13 @@ for cond in ["amph", "veh"]:
     rate_mean_smooth /= np.max(rate_mean_smooth)
     velocity_smooth /= np.max(velocity_smooth)
 
-    # calculate change in velocity and angle
+    # calculate changes in smoothed variables
     acceleration = np.diff(velocity_smooth, 1)
     acceleration /= np.max(acceleration)
     angle_change = np.diff(angle_smooth, 1)
     angle_change /= np.max(angle_change)
+    rate_change = np.diff(rate_mean_smooth, 1)
+    rate_change /= np.max(rate_change)
     acceleration[acceleration > 0] = np.sqrt(acceleration[acceleration > 0])
     acceleration[acceleration < 0] = -np.sqrt(-acceleration[acceleration < 0])
     angle_change[angle_change > 0] = np.sqrt(angle_change[angle_change > 0])
@@ -80,6 +83,7 @@ for cond in ["amph", "veh"]:
     results["condition"].extend([cond] * n)
     results["time"].extend(np.arange(0, n, dtype=np.float64) / sr)
     results["rate_mean"].extend(rate_mean_smooth[:n].tolist())
+    results["rate_change"].extend(rate_change[:n].tolist())
     results["velocity"].extend(velocity_smooth[:n].tolist())
     results["acceleration"].extend(acceleration[:n].tolist())
     results["angle"].extend(angle_smooth[:n].tolist())
@@ -91,8 +95,8 @@ results = DataFrame.from_dict(results)
 # plotting
 ##########
 
-fig = plt.figure(figsize=(12, 4))
-grid = fig.add_gridspec(nrows=2, ncols=2)
+fig = plt.figure(figsize=(6, 7))
+grid = fig.add_gridspec(nrows=5, ncols=2)
 titles = ["Control condition", "Dopamine condition"]
 window = [200, 500]
 state_colors = ["black", "darkred", "darkgreen"]
@@ -107,21 +111,23 @@ for i, cond in enumerate(["veh", "amph"]):
     x = results_tmp.loc[:, "time"].values
 
     # movement
-    ax = fig.add_subplot(grid[0, i])
+    ax = fig.add_subplot(grid[i*2, :])
     ax.plot(x, results_tmp.loc[:, "acceleration"], color=c1)
     ax2 = ax.twinx()
     ax2.plot(x, results_tmp.loc[:, "angle_change"], color=c2)
     if i == 0:
         ax.set_ylabel("acceleration", color=c1)
     else:
-        ax2.set_ylabel("change in HNT angle", color=c2)
+        ax2.set_ylabel("HNT change", color=c2)
     ax.set_title(titles[i])
 
     # neural rates
-    ax = fig.add_subplot(grid[1, i])
+    ax = fig.add_subplot(grid[i*2+1, :])
     mean_rate = results_tmp.loc[:, "rate_mean"]
     mean_rate /= np.max(mean_rate)
-    ax.plot(x, mean_rate, color=c1, label="mean rate")
+    ax.plot(x, mean_rate, color=c1)
+    ax2 = ax.twinx()
+    ax2.plot(x, results_tmp.loc[:, "rate_change"], color=c2)
     borders = np.argwhere(np.abs(np.diff(state, 1)) > 0.0).squeeze().tolist() + [len(state)-1]
     b0 = 0
     for b in borders:
@@ -131,8 +137,26 @@ for i, cond in enumerate(["veh", "amph"]):
         b0 = b
     # ax.legend()
     if i == 0:
-        ax.set_ylabel("norm. spike rate")
+        ax.set_ylabel("spike rate (SR)")
+    else:
+        ax2.set_ylabel("SR change", color=c2)
     ax.set_xlabel("time (s)")
+
+# bar graph: spike rates
+ax = fig.add_subplot(grid[4, 0])
+sns.barplot(results, x="state", y="rate_mean", hue="condition", ax=ax, palette="dark")
+ax.set_xlabel("behavioral state")
+ax.set_ylabel("SR")
+ax.set_xlim([0.5, 3.5])
+ax.set_xticks([1, 2, 3], labels=["rest", "dec.", "acc."])
+
+# bar graph: rate change
+ax = fig.add_subplot(grid[4, 1])
+sns.barplot(results, x="state", y="rate_change", hue="condition", ax=ax, palette="dark")
+ax.set_xlabel("behavioral state")
+ax.set_ylabel("SR change")
+ax.set_xlim([0.5, 3.5])
+ax.set_xticks([1, 2, 3], labels=["rest", "dec.", "acc."])
 
 # padding
 fig.set_constrained_layout_pads(w_pad=0.03, h_pad=0.01, hspace=0.01, wspace=0.01)
