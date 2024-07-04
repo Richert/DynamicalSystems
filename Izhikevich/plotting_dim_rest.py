f814f3f38d76e48a64deb4c870656cc873626d0e
Richert <richgast.90@gmail.com>
2024-07-04 15:16:46 -0500

    new scripts for fitting a combination of Lorentzians to the recovery variables in SNNs

---
+++ Izhikevich/plotting_dim_rest.py
@@ -0,0 +1,48 @@
+import pickle
+from seaborn import heatmap, lineplot
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+from pandas import DataFrame
+
+
+# load data
+path = "/home/rgf3807/PycharmProjects/DynamicalSystems/Izhikevich/results/snn_dim"
+results = {"rep": [], "g": [], "Delta": [], "dim": [], "s_mean": [], "s_std": [], "s_norm": []}
+for file in os.listdir(path):
+    data = pickle.load(open(f"{path}/{file}", "rb"))
+    results["rep"].append(int(file.split("_")[-1][:-2]))
+    results["g"].append(data["g"])
+    results["Delta"].append(data["Delta"])
+    results["dim"].append(data["dim"])
+    results["s_mean"].append(np.mean(data["s_mean"]))
+    results["s_std"].append(np.mean(data["s_std"]))
+    results["s_norm"].append(results["s_std"][-1]/results["s_mean"][-1])
+
+# create dataframe
+df = DataFrame.from_dict(results)
+
+# reshape results into 2D tables
+dim = df.pivot_table(values="dim", index="g", columns="Delta")
+fr_std = df.pivot_table(values="s_std", index="g", columns="Delta")
+fr_norm = df.pivot_table(values="s_norm", index="g", columns="Delta")
+
+# plotting 2D plots
+fig, axes = plt.subplots(ncols=3, figsize=(12, 4))
+for ax, data, title in zip(axes, [dim, fr_std, fr_norm], ["Participation Ratio", "std(fr)", "std(fr)/mean(fr)"]
+                           ):
+    heatmap(data, ax=ax)
+    ax.set_title(title)
+plt.tight_layout()
+plt.show()
+
+# plotting 1D plots
+fig, axes = plt.subplots(ncols=3, figsize=(12, 4))
+ax = axes[0]
+lineplot(df, x="Delta", y="dim", hue="g", ax=ax)
+ax = axes[1]
+lineplot(df, x="s_norm", y="dim", hue="g", ax=ax)
+ax = axes[2]
+lineplot(df, x="s_std", y="dim", hue="g", ax=ax)
+plt.tight_layout()
+plt.show()
---
+++ Izhikevich/simulation_snn_dim_ir.py
@@ -0,0 +1,114 @@
+from rectipy import Network, random_connectivity
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from scipy.stats import cauchy, norm
+import sys
+
+
+def lorentzian(n: int, eta: float, delta: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = cauchy.rvs(loc=eta, scale=delta)
+        while s <= lb or s >= ub:
+            s = cauchy.rvs(loc=eta, scale=delta)
+        samples[i] = s
+    return samples
+
+
+def gaussian(n, mu: float, delta: float, lb: float, ub: float):
+    samples = np.zeros((n,))
+    for i in range(n):
+        s = norm.rvs(loc=mu, scale=delta)
+        while s <= lb or s >= ub:
+            s = norm.rvs(loc=mu, scale=delta)
+        samples[i] = s
+    return samples
+
+
+# define parameters
+###################
+
+# get sweep condition
+rep = int(sys.argv[-1])
+g = float(sys.argv[-2])
+Delta = float(sys.argv[-3])
+
+# model parameters
+N = 1000
+p = 0.2
+C = 100.0
+k = 0.7
+v_r = -60.0
+v_t = -40.0
+eta = 50.0
+a = 0.03
+b = -2.0
+d = 100.0
+E_r = 0.0
+tau_s = 6.0
+v_spike = 1000.0
+v_reset = -1000.0
+theta_dist = "gaussian"
+
+# define distribution of etas
+f = gaussian if theta_dist == "gaussian" else lorentzian
+thetas = f(N, mu=v_t, delta=Delta, lb=v_r, ub=2*v_t-v_r)
+
+# define connectivity
+W = random_connectivity(N, N, p, normalize=True)
+
+# define inputs
+T = 2500.0
+cutoff = 500.0
+start = 500.0
+stop = 510.0
+amp = 100.0
+dt = 1e-2
+dts = 1e-1
+inp = np.zeros((int(T/dt), 1))
+inp[int(start/dt):int(stop/dt), 0] += amp
+
+# run the model
+###############
+
+# initialize model
+node_vars = {"C": C, "k": k, "v_r": v_r, "v_theta": thetas, "eta": eta, "tau_u": 1/a, "b": b, "kappa": d,
+             "g_e": g, "E_e": E_r, "tau_s": tau_s, "v": v_t, "g_i": 0.0}
+
+# initialize model
+net = Network(dt, device="cpu")
+net.add_diffeq_node("ik", f"config/ik_snn/ik", weights=W, source_var="s", target_var="s_e",
+                    input_var="I_ext", output_var="s", spike_var="spike", reset_var="v", to_file=False,
+                    node_vars=node_vars.copy(), op="ik_op", spike_reset=v_reset, spike_threshold=v_spike,
+                    clear=True)
+
+# perform simulation
+obs = net.run(inputs=inp, sampling_steps=int(dts/dt), record_output=True, verbose=False, enable_grad=False,
+              cutoff=int(cutoff/dt))
+s = obs.to_numpy("out")
+
+# calculate dimensionality
+cov = s.T @ s
+eigs = np.abs(np.linalg.eigvals(cov))
+dim = np.sum(eigs) ** 2 / np.sum(eigs ** 2)
+
+# calculate firing rate statistics
+s_mean = np.mean(s, axis=1)
+s_std = np.std(s, axis=1)
+
+# save results
+pickle.dump({"g": g, "Delta": Delta, "theta_dist": theta_dist, "dim": dim, "s_mean": s_mean, "s_std": s_std,
+             "tau_s": tau_s, "N": N},
+            open(f"results/snn_ir_g{int(g)}_D{int(Delta*10)}_{rep+1}.p", "wb"))
+
+# # plotting
+# fig, ax = plt.subplots(figsize=(12, 4))
+# ax.plot(s_mean*1e3/tau_s, label="mean(r)")
+# ax.plot(s_std*1e3/tau_s, label="std(r)")
+# ax.legend()
+# ax.set_xlabel("steps")
+# ax.set_ylabel("r")
+# ax.set_title(f"Dim = {dim}")
+# plt.tight_layout()
+# plt.show()
