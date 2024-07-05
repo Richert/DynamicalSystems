b1d8c326da747734832b8e10760d4d5d65ea18ea
Richert <richgast.90@gmail.com>
2024-07-05 09:11:17 -0500

    new scripts for fitting a combination of Lorentzians to the recovery variables in SNNs

---
+++ Izhikevich/scheduler_dim_ir.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# set condition
+deltas=( 1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 )
+gs=( 0.0 4.0 8.0 12.0 16.0 20.0 24.0 28.0 32.0 )
+n=20
+batch_size=8
+range_end=$(($n-1))
+
+# limit amount of threads that each Python process can work with
+n_threads=10
+export OMP_NUM_THREADS=$n_threads
+export OPENBLAS_NUM_THREADS=$n_threads
+export MKL_NUM_THREADS=$n_threads
+export NUMEXPR_NUM_THREADS=$n_threads
+export VECLIB_MAXIMUM_THREADS=$n_threads
+
+# execute python scripts in batches of batch_size
+for d in "${deltas[@]}"; do
+  for g in "${gs[@]}"; do
+    for IDX in `seq 0 $range_end`; do
+
+      # python calls
+      (
+      echo "Starting job #$(($IDX+1)) of ${n} jobs for g = ${g} and delta = ${d}."
+      python simulation_snn_dim_ir.py $d $g $IDX
+      sleep 1
+      ) &
+
+      # batch control
+      if [[ $(jobs -r -p | wc -l) -ge $batch_size ]]; then
+            wait -n
+      fi
+
+    done
+  done
+done
+
+wait
+echo "All jobs finished."
