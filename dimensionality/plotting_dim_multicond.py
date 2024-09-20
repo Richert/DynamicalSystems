f9f26b7aaf6d1dbf379645d629758eaee265fd07
Richert <richgast.90@gmail.com>
2024-09-20 13:06:41 -0700

    new dimensionality calculations

--- dimensionality/plotting_dim_multicond.py
+++ dimensionality/plotting_dim_multicond.py
@@ -16,7 +16,7 @@ plt.rcParams['font.size'] = 10.0
 plt.rcParams['axes.titlesize'] = 10
 plt.rcParams['axes.labelsize'] = 10
 plt.rcParams['lines.linewidth'] = 1.0
-markersize = 2.0
+markersize = 1.0
 
 # condition
 path = str(sys.argv[1])
