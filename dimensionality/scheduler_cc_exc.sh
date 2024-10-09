b20b605c2cf0a54d6b3d2be8c14dd6cc7b80e033
Richert <richgast.90@gmail.com>
2024-10-09 09:19:50 -0700

    new dimensionality calculations

--- dimensionality/scheduler_cc_exc.sh
+++ dimensionality/scheduler_cc_exc.sh
@@ -5,7 +5,7 @@ deltas=( 0.1 0.2 0.4 0.8 1.6 3.2 6.4 )
 gs=( 0.0 0.3 0.6 0.9 1.2 1.5 1.8 )
 ps=( 0.05 0.1 0.2 )
 n=20
-batch_size=20
+batch_size=10
 range_end=$((n-1))
 
 # directories
@@ -20,7 +20,6 @@ export NUMEXPR_NUM_THREADS=$n_threads
 export VECLIB_MAXIMUM_THREADS=$n_threads
 
 # execute python scripts in batches of batch_size
-# execute python scripts
 for p in "${ps[@]}"; do
   for d in "${deltas[@]}"; do
     for g in "${gs[@]}"; do
