af47a9943a5a5d2d103fa170137f57ab591e085a
Richert <richgast.90@gmail.com>
2024-12-28 10:26:34 -0800

    new dimensionality calculations

--- dimensionality/postprocessing_dim.py
+++ dimensionality/postprocessing_dim.py
@@ -4,13 +4,18 @@ import numpy as np
 from pandas import DataFrame
 
 # condition
-iv = "Delta_i"
-condition = "dim2_eic"
+iv = "ei_ratio"
+condition = "dim_eir"
 path = "/media/richard/results/dimensionality"
 
 # load data
-results = {"rep": [], "g": [], "Delta": [], iv: [], "dim_ss": [], "s_mean": [], "s_std": [], "s_norm": [],
-           "dim_ir": [], "tau_ir": [], "offset_ir": [], "amp_ir": [], "dim_ir_nc": [], "dim_ss_nc": []}
+results = {"rep": [], "g": [], "Delta": [], iv: [],
+           "s_mean": [], "s_std": [], "s_norm": [], "ff_mean": [], "ff_max": [],
+           "dim_ss": [], "dim_ss_c": [], "dim_ss_r": [], "dim_ss_rc": [],
+           "dim_ir": [], "dim_ir_c": [], "dim_ir_r": [], "dim_ir_rc": [],
+           "dim_sep": [], "dim_sep_c": [], "dim_sep_r": [], "dim_sep_rc": [],
+           "tau_ir": [], "offset_ir": [], "amp_ir": [],
+           "tau_k": [], "mean_k": [], "std_k": []}
 for file in os.listdir(path):
     if file[:len(condition)] == condition:
 
@@ -28,18 +33,24 @@ for file in os.listdir(path):
             results[iv].append(data[iv])
 
             # steady-state analysis
-            results["dim_ss"].append(data["dim_ss"])
-            results["dim_ss_nc"].append(data["dim_ss_nc"])
             results["s_mean"].append(np.mean(data["s_mean"])*1e3)
             results["s_std"].append(np.mean(data["s_std"]))
             results["s_norm"].append(results["s_std"][-1]*1e3/results["s_mean"][-1])
 
+            # dimensionality analysis
+            for c1 in ["ss", "ir", "sep"]:
+                for c2 in ["", "_c", "_r", "_rc"]:
+                    results[f"dim_{c1 + c2}"].append(data[f"dim_{c1 + c2}"])
+
             # impulse response analysis
-            results["dim_ir"].append(data["dim_ir"])
-            results["dim_ir_nc"].append(data["dim_ir_nc"])
             results["tau_ir"].append(data["params_ir"][-2])
             results["offset_ir"].append(data["params_ir"][0])
             results["amp_ir"].append(data["params_ir"][2])
+
+            # kernel analysis
+            results["tau_k"].append(data["params_kernel"][-2])
+            results["amp_k"].append(np.mean(data["K_diag"]))
+            results["std_k"].append(np.std(data["K_diag"]))
 
 # create dataframe
 df = DataFrame.from_dict(results)
