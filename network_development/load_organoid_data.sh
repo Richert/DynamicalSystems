c7b050b927f26e7a19cc03f3a0edf358adbab90e
Richert <richgast.90@gmail.com>
2025-01-21 14:47:11 -0800

    new dimensionality calculations

--- network_development/load_organoid_data.sh
+++ network_development/load_organoid_data.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+# files, directories and names
 file_names=( "LFP_Sp_160731" "LFP_Sp_160803" )
 file_ending="mat"
 url_address="https://zenodo.org/records/4751759"
 target_dir="/media/richard/data/trujilo_2019"
 
+# load data
 for f in "${file_names}"; do
   curl --output "${target_dir}/${f}.${file_ending}" "${url_address}/${f}.${file_ending}"
 done
