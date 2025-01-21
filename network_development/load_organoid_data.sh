bfe50c243f6088c88e4ab5a4a62d55dff5ea3252
Richert <richgast.90@gmail.com>
2025-01-21 14:41:34 -0800

    new dimensionality calculations

--- network_development/load_organoid_data.sh
+++ network_development/load_organoid_data.sh
@@ -4,5 +4,5 @@ url_address = "https://zenodo.org/records/4751759"
 target_dir = "/media/richard/data/trujilo_2019"
 
 for f in "${file_names}"; do
-  curl --output "${target_dir}/${f}.${file_ending}" "${url_address}/${f}.${file_ending}?download=1"
+  curl --output "${target_dir}/${f}.${file_ending}" "${url_address}/${f}.${file_ending}"
 done
