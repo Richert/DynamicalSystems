f8f4acc9dcbb1df8cd9839d3d7ffc858dfefd1b3
Richert <richgast.90@gmail.com>
2025-01-21 14:43:12 -0800

    new dimensionality calculations

--- network_development/load_organoid_data.sh
+++ network_development/load_organoid_data.sh
@@ -1,7 +1,7 @@
 file_names=("LFP_Sp_160731", "LFP_Sp_160803")
-file_ending = "mat"
-url_address = "https://zenodo.org/records/4751759"
-target_dir = "/media/richard/data/trujilo_2019"
+file_ending="mat"
+url_address="https://zenodo.org/records/4751759"
+target_dir="/media/richard/data/trujilo_2019"
 
 for f in "${file_names}"; do
   curl --output "${target_dir}/${f}.${file_ending}" "${url_address}/${f}.${file_ending}"
