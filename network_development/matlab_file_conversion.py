237b48c96aee98b6e542e6c4f1bb34c61410ffde
Richert <richgast.90@gmail.com>
2025-01-22 10:00:26 -0800

    new dimensionality calculations

--- network_development/matlab_file_conversion.py
+++ network_development/matlab_file_conversion.py
@@ -7,5 +7,6 @@ wdir = os.getcwd()
 oct.eval(f"cd {path}")
 for f in os.listdir(path):
     if f.split(".")[-1] == "mat":
+        oct.eval(f"load {f}")
         oct.eval(f"save -v7 {f}")
 oct.eval(f"cd {wdir}")
