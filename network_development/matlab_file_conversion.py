0a1c391aac0b02cf949167013f6da50a8b70f183
Richert <richgast.90@gmail.com>
2025-01-22 15:17:12 -0800

    new dimensionality calculations

--- network_development/matlab_file_conversion.py
+++ network_development/matlab_file_conversion.py
@@ -3,11 +3,14 @@ import os
 
 path = "/home/richard/data/trujilo_2019"
 wdir = os.getcwd()
+target_format = "v6"
 
 # load data and convert it to the hdf5 format
 oct.eval(f"cd {path}")
 for f in os.listdir(path):
     if f.split(".")[-1] == "mat":
+        print(f"Converting file {f} to {target_format}...")
         oct.eval(f"load {f}")
-        oct.eval(f"save -v6 {f}")
+        oct.eval(f"save -{target_format} {f}")
+print("Finished the conversion process.")
 oct.eval(f"cd {wdir}")
