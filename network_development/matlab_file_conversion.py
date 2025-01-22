747ed16878b647a2ba372a6d79c59ffb59c61ab6
Richert <richgast.90@gmail.com>
2025-01-22 15:28:22 -0800

    new dimensionality calculations

--- network_development/matlab_file_conversion.py
+++ network_development/matlab_file_conversion.py
@@ -1,4 +1,5 @@
 from oct2py import octave as oct
+from oct2py.utils import Oct2PyError
 import os
 
 path = "/home/richard/data/trujilo_2019"
@@ -9,8 +10,13 @@ target_format = "hdf5"
 oct.eval(f"cd {path}")
 for f in os.listdir(path):
     if f.split(".")[-1] == "mat":
-        print(f"Converting file {f} to {target_format}...")
-        oct.eval(f"load {f}")
-        oct.eval(f"save -{target_format} {f}")
+        try:
+            print(f"Converting file {f} to {target_format}...")
+            oct.eval(f"load {f}")
+            oct.eval(f"save -{target_format} {f}")
+        except Oct2PyError as e:
+            print(f"The following error occured trying to convert {f}:")
+            print(e)
+            pass
 print("Finished the conversion process.")
 oct.eval(f"cd {wdir}")
