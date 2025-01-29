1cb01f4e0919c38e7ba05b907d2c4a96bb461e78
Richert <richgast.90@gmail.com>
2025-01-28 17:09:13 -0800

    new dimensionality calculations

--- network_development/plotting_organoid_development.py
+++ network_development/plotting_organoid_development.py
@@ -6,7 +6,7 @@ from pandas import read_csv
 # load data
 ###########
 
-dataset = "organoid_dynamics"
+dataset = "trujilo_2019"
 path = f"/home/richard-gast/Documents/data/{dataset}"
 file = f"{dataset}_summary.csv"
 data = read_csv(f"{path}/{file}")
@@ -18,5 +18,5 @@ dvs = ["dim", "spike_reg", "ff", "burst_freq", "burst_reg", "rate_avg", "rate_he
        "intraburst_ff", "intraburst_freq", "intraburst_spike_reg", "intraburst_rate_avg", "intraburst_rate_het"]
 for y in dvs:
     fig, ax= plt.subplots(figsize=(8, 4))
-    sb.lineplot(data, x="age", y=y, hue="organoid", ax=ax)
+    sb.lineplot(data, x="age", y=y, errorbar="sd", ax=ax)
 plt.show()
