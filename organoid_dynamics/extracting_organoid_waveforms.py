752477dcf0e0c23ca027045532537544501a6ef2
Richert <richgast.90@gmail.com>
2025-03-07 08:09:37 -0800

    new dimensionality calculations

--- organoid_dynamics/extracting_organoid_waveforms.py
+++ organoid_dynamics/extracting_organoid_waveforms.py
@@ -74,7 +74,7 @@ if __name__ == '__main__':
     # save results to file
     ######################
 
-    df = DataFrame(data=np.asarray(list(results.values())),
+    df = DataFrame(data=np.asarray(list(results.values())).T,
                    columns=MultiIndex.from_tuples(results.keys()),
                    index=np.arange(waveform_length))
     df.to_csv(f"{path}/{dataset_name}_waveforms.csv")
