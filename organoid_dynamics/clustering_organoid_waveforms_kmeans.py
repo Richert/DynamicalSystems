6adbaf31dca57c6eb83539c0395b12cff89c46c2
Richert <richgast.90@gmail.com>
2025-03-14 10:02:24 -0700

    new dimensionality calculations

--- organoid_dynamics/clustering_organoid_waveforms_kmeans.py
+++ organoid_dynamics/clustering_organoid_waveforms_kmeans.py
@@ -10,9 +10,9 @@ from custom_functions import *
 
 
 # read in data
-path = "/home/richard/data"
+path = "/home/richard"
 dataset = "trujilo_2019"
-data = pd.read_csv(f"{path}/{dataset}/{dataset}_waveforms.csv",
+data = pd.read_csv(f"{path}/data/{dataset}/{dataset}_waveforms.csv",
                    header=[0, 1, 2], index_col=0)
 
 # reduce data
@@ -35,31 +35,31 @@ km = TimeSeriesKMeans(n_clusters=6, max_iter=100, tol=1e-6, n_init=3, metric="dt
 km.fit(data_norm)
 
 # calculate distance between waves
-calc_dists = False
+calc_dists = True
 if calc_dists:
     D = cdist_dtw(data_norm[:, :], n_jobs=n_jobs)
-    np.save(f"{path}/{dataset}/{dataset}_waveform_distances_norm", D)
+    np.save(f"{path}/results/{dataset}/{dataset}_waveform_distances", D)
 else:
-    D = np.load(f"{path}/{dataset}/{dataset}_waveform_distances_norm.npy")
+    D = np.load(f"{path}/results/{dataset}/{dataset}_waveform_distances.npy")
 
 # save prototypical waveforms
 proto_waves = np.asarray(km.cluster_centers_).squeeze()
-np.save(f"{path}/{dataset}/{dataset}_cluster_centers_kmeans.npy", proto_waves)
+np.save(f"{path}/results/{dataset}/{dataset}_cluster_centers_kmeans.npy", proto_waves)
 
-# plot prototypical waveforms
-fig, ax = plt.subplots(figsize=(12, 5))
-for sample in range(proto_waves.shape[0]):
-    ax.plot(proto_waves[sample], label=sample)
-ax.set_ylabel("firing rate")
-ax.set_xlabel("time (ms)")
-ax.legend()
-ax.set_title("Prototypical waveforms fur clusters")
-plt.tight_layout()
-
-# plot clustering results
-fig, ax = plt.subplots(figsize=(10, 8))
-D = D[km.labels_, :]
-D = D[:, km.labels_]
-im = ax.imshow(D, interpolation="none", aspect="equal", cmap="cividis")
-plt.colorbar(im, ax=ax)
-plt.show()
+# # plot prototypical waveforms
+# fig, ax = plt.subplots(figsize=(12, 5))
+# for sample in range(proto_waves.shape[0]):
+#     ax.plot(proto_waves[sample], label=sample)
+# ax.set_ylabel("firing rate")
+# ax.set_xlabel("time (ms)")
+# ax.legend()
+# ax.set_title("Prototypical waveforms fur clusters")
+# plt.tight_layout()
+#
+# # plot clustering results
+# fig, ax = plt.subplots(figsize=(10, 8))
+# D = D[km.labels_, :]
+# D = D[:, km.labels_]
+# im = ax.imshow(D, interpolation="none", aspect="equal", cmap="cividis")
+# plt.colorbar(im, ax=ax)
+# plt.show()
