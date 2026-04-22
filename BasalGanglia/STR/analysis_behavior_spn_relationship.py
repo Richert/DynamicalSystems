3d7e6174cc04eea6603037cad3d00e97b684cdba
Richert <richgast.90@gmail.com>
2026-04-22 13:39:37 -0700

    new stuff

--- BasalGanglia/STR/analysis_behavior_spn_relationship.py
+++ BasalGanglia/STR/analysis_behavior_spn_relationship.py
@@ -98,7 +98,7 @@ for drug in drugs:
             s2 = np.asarray([gaussian_filter1d(s[i, :], sigma=sigma_rate) for i in range(s.shape[0])])
             v2 = gaussian_filter1d(v, sigma=sigma_speed)
             a2 = np.diff(v2)
-            v2 = v2[:-1]
+            v2, s2 = v2[:-1], s2[:, :-1]
 
             # calculate result variables and save data
             for b, thresholds in behaviors.items():
